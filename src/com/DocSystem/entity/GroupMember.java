c0055649b9b364300a579aa57cf6992e5ca061e6
Rainy <652055239@qq.com>
2018-03-13 16:14:01 +0800

    增加UserGroup, GroupMember表， reposAuth和docAuth增加GroupID字段

---
+++ src/com/DocSystem/entity/GroupMember.java
@@ -0,0 +1,33 @@
+package com.DocSystem.entity;
+
+public class GroupMember {
+    private Integer id;
+
+    private Integer groupId;
+
+    private Integer userId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
---
+++ src/com/DocSystem/entity/UserGroup.java
@@ -0,0 +1,73 @@
+package com.DocSystem.entity;
+
+public class UserGroup {
+    private Integer id;
+
+    private String name;
+
+    private Integer type;
+
+    private String info;
+
+    private String img;
+
+    private Integer priority;
+
+    private String createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info == null ? null : info.trim();
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img == null ? null : img.trim();
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+}
