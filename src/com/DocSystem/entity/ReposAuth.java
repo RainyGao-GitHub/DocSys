202e184a9bbe7192f594890a5a67e1d1bde2b42b
Rainy <652055239@qq.com>
2018-03-14 14:44:55 +0800

    delete UserDocAuth and move these info to reposAuth and docAuth

--- src/com/DocSystem/entity/ReposAuth.java
+++ src/com/DocSystem/entity/ReposAuth.java
@@ -101,6 +101,7 @@ public class ReposAuth {
         this.heritable = heritable;
     }
     
+    //1: 用户直接权限  2: 用户所在组权限  3: 任意用户权限
     private Integer type = 0;
     public Integer getType() {
         return type;
@@ -108,4 +109,43 @@ public class ReposAuth {
     public void setType(Integer type) {
         this.type = type;
     }
+    
+    //ReposAuth 附加信息: 需要通过joint doc and user or group Table to get these info
+    private String  userName="任意用户";	//from user Table
+    private String  groupName="";	//from group Table
+    
+    private String docName="";		//from doc Table
+    private String docPath="";		//from doc Table
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+    
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName == null ? null : groupName.trim();
+    }
+   
+    public String getDocName() {
+        return docName;
+    }
+
+    public void setDocName(String docName) {
+        this.docName = docName;
+    }
+    
+    public String getDocPath() {
+        return docPath;
+    }
+
+    public void setDocPath(String docPath) {
+        this.docPath = docPath;
+    }   
 }
