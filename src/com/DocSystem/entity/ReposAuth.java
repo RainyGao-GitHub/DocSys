39dd70f47e55b6f7a30e4f426e58f4876d824690
Rainy <652055239@qq.com>
2018-03-22 14:26:02 +0800

    docAuth reposAuth add type priority

--- src/com/DocSystem/entity/ReposAuth.java
+++ src/com/DocSystem/entity/ReposAuth.java
@@ -7,6 +7,10 @@ public class ReposAuth {
 
     private Integer groupId;
 
+    private Integer type;
+
+    private Integer priority;
+
     private Integer reposId;
 
     private Integer isAdmin;
@@ -43,6 +47,22 @@ public class ReposAuth {
 
     public void setGroupId(Integer groupId) {
         this.groupId = groupId;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
     }
 
     public Integer getReposId() {
