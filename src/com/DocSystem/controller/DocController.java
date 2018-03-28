3ef1af56c8c2bf6924d996cc20f742b60fb41618
Rainy <652055239@qq.com>
2018-03-28 11:18:44 +0800

    Fix the synclock bug

--- src/com/DocSystem/controller/DocController.java
+++ src/com/DocSystem/controller/DocController.java
@@ -797,12 +797,13 @@ public class DocController extends BaseController{
 	}	
 	private void unlockSyncLock(Object syncLock) {
 		syncLock.notifyAll();//唤醒等待线程
-		try {
-			syncLock.wait();	//释放锁
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+		//下面这段代码是因为参考了网上的一个Demo说wait是释放锁，我勒了个区去，留着作纪念
+		//try {
+		//	syncLock.wait();	//线程睡眠，等待syncLock.notify/notifyAll唤醒
+		//} catch (InterruptedException e) {
+		//	e.printStackTrace();
+		//}
+	}  
 
 	//底层deleteDoc接口
 	private void deleteDoc(Integer docId, Integer reposId,Integer parentId, 
