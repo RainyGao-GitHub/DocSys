f37583a0e47ed4cdc10468beb8cd20ab3e9781c2
Rainy <652055239@qq.com>
2018-05-18 15:18:18 +0800

    新增管理后台的controller

---
+++ src/com/DocSystem/controller/ManageController.java
@@ -0,0 +1,67 @@
+package com.DocSystem.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.tmatesoft.svn.core.SVNException;
+
+import util.ReturnAjax;
+import util.SvnUtil.SVNUtil;
+
+import com.DocSystem.entity.DocAuth;
+import com.DocSystem.entity.Repos;
+import com.DocSystem.entity.Doc;
+import com.DocSystem.entity.User;
+import com.DocSystem.entity.ReposAuth;
+import com.DocSystem.entity.UserGroup;
+
+import com.DocSystem.service.UserService;
+import com.DocSystem.service.impl.ReposServiceImpl;
+import com.DocSystem.service.impl.UserServiceImpl;
+
+import com.DocSystem.controller.BaseController;
+import com.alibaba.fastjson.JSON;
+
+@Controller
+@RequestMapping("/Manage")
+public class ManageController extends BaseController{
+	@Autowired
+	private UserServiceImpl userService;
+	
+	/********** 获取用户列表 ***************/
+	@RequestMapping("/getUserList.do")
+	public void getUserList(HttpSession session,HttpServletRequest request,HttpServletResponse response)
+	{
+		System.out.println("getUserList()");
+		ReturnAjax rt = new ReturnAjax();
+		User login_user = (User) session.getAttribute("login_user");
+		if(login_user == null)
+		{
+			rt.setError("用户未登录，请先登录！");
+			writeJson(rt, response);			
+			return;
+		}
+		
+		//获取All UserList
+		List <User> UserList = getAllUsers();
+		
+		rt.setData(UserList);
+		writeJson(rt, response);
+	}
+
+	private List<User> getAllUsers() {
+		List <User> UserList = userService.geAllUsers();
+		return UserList;
+	}
+
+}
