fd632f447a9c5e148d7688780a01fffe3c6f8eae
Rainy <652055239@qq.com>
2018-06-12 11:09:20 +0800

    openoffice test

---
+++ src/OpenOfficeTest.java
@@ -0,0 +1,9 @@
+import util.DocConvertUtil.Office2PDF;
+
+class OpenOfficeTest  
+{  
+
+    public static void main(String[] args) {
+        Office2PDF.openOfficeToPDF("/Users/JJC/Downloads/20170302汽修服务测试反馈.docx");
+    }
+}
