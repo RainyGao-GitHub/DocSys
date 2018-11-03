1d294e4366920fc6412fb4f197c82d6f8a47e6d8
RainyGao <652055239@qq.com>
2018-11-03 16:41:05 +0800

    为文件添加indexok，search时有点问题

--- src/util/LuceneUtil2.java
+++ src/util/LuceneUtil2.java
@@ -3,13 +3,21 @@
  */
 package util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.IntField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
@@ -18,19 +26,24 @@ import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
+import com.sun.star.io.IOException;
+
 /**  
  * 类描述：   lucene索引增删改查的公共类
- * 创建人：zhanjp
- * 创建时间：2015-12-7 上午9:24:32
+ * 创建人：高雨
+ * 创建时间：2018-10-1
  * @version    
- *    
+ * This class is the full-text search driver for DocSys
+ * There are multi-lucence document mapped to one doc in DocSys, if the content of this Doc is very large    
  */
 public class LuceneUtil2 {
 
@@ -39,18 +52,6 @@ public class LuceneUtil2 {
     private static Analyzer analyzer = null;
     private static Directory directory = null;
     private static IndexWriter indexWriter = null;
-
-    public static void main(String[] args) {
-        try {
-//            index();
-            search("man","user");
-//            insert();
-//            delete("text5");
-//            update();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     
     private static String getLucenePath() {
 		String path = ReadProperties.read("docSysConfig.properties", "lucenePath");
@@ -78,18 +79,53 @@ public class LuceneUtil2 {
 		}	 
 		return path;
 	}
+    
+	/**
+     * 增加索引
+     * @param id: lucence document id
+     * @param docId:  docId of DocSys 
+     * @param content: 文件内容或markdown文件内容 
+     * @param type: 索引库名字
+     */
+    @SuppressWarnings("deprecation")
+	public static void addIndex(String id,Integer docId, String content,String type) throws Exception {
+    	
+    	System.out.println("addIndex() id:" + id + " docId:"+ docId + " indexLib:"+type + " content:" + content);
+    	
+    	Date date1 = new Date();
+        analyzer = new IKAnalyzer();
+        directory = FSDirectory.open(new File(INDEX_DIR + File.separator+ type));
+
+        IndexWriterConfig config = new IndexWriterConfig(
+                Version.LUCENE_CURRENT, analyzer);
+        indexWriter = new IndexWriter(directory, config);
+
+        Document doc = new Document();
+        doc.add(new TextField("id", id, Store.YES));
+        doc.add(new IntField("docId", docId, Store.YES));
+        doc.add(new TextField("content", content, Store.YES));
+        indexWriter.addDocument(doc);
+        
+        indexWriter.commit();
+        indexWriter.close();
+
+        Date date2 = new Date();
+        System.out.println("创建索引耗时：" + (date2.getTime() - date1.getTime()) + "ms\n");
+    }
 
 	/**
      * 更新索引
-     * @param id
-     * @param content
-     * @param type
-     * @throws Exception
+     * @param id: lucence document id
+     * @param docId:  docId of DocSys 
+     * @param content: 文件内容或markdown文件内容 
+     * @param type: 索引库名字
      */
     @SuppressWarnings("deprecation")
-	public static void update(String id,String content,String type) throws Exception {
-//        String text1 = "update,hello,man!";
-        Date date1 = new Date();
+	public static void updateIndex(String id,Integer docId,String content,String type) throws Exception {
+
+    	System.out.println("updateIndex() id:" + id + " docId:"+ docId + " indexLib:"+type + " content:" + content);
+    	
+    	Date date1 = new Date();
         analyzer = new IKAnalyzer();
         directory = FSDirectory.open(new File(INDEX_DIR + File.separator + type));
 
@@ -99,6 +135,7 @@ public class LuceneUtil2 {
          
         Document doc1 = new Document();
         doc1.add(new TextField("id", id, Store.YES));
+        doc1.add(new IntField("docId", docId, Store.YES));
         doc1.add(new TextField("content", content, Store.YES));
         
         indexWriter.updateDocument(new Term("id",id), doc1);
@@ -115,7 +152,7 @@ public class LuceneUtil2 {
      * @throws Exception
      */
     @SuppressWarnings("deprecation")
-	public static void delete(String id,String type) throws Exception {
+	public static void deleteIndex(String id,String type) throws Exception {
         Date date1 = new Date();
         analyzer = new IKAnalyzer();
         directory = FSDirectory.open(new File(INDEX_DIR + File.separator + type));
@@ -130,77 +167,14 @@ public class LuceneUtil2 {
         
         Date date2 = new Date();
         System.out.println("删除索引耗时：" + (date2.getTime() - date1.getTime()) + "ms\n");
-    }
-    
-    /**
-     * 增加索引
-     * 
-     * @throws Exception
-     */
-    @SuppressWarnings("deprecation")
-	public static void insert() throws Exception {
-        String text5 = "hello,goodbye,man,woman";
-        Date date1 = new Date();
-        analyzer = new IKAnalyzer();
-        directory = FSDirectory.open(new File(INDEX_DIR));
-
-        IndexWriterConfig config = new IndexWriterConfig(
-                Version.LUCENE_CURRENT, analyzer);
-        indexWriter = new IndexWriter(directory, config);
-
-        Document doc1 = new Document();
-        doc1.add(new TextField("filename", "text5", Store.YES));
-        doc1.add(new TextField("content", text5, Store.YES));
-        indexWriter.addDocument(doc1);
-
-        indexWriter.commit();
-        indexWriter.close();
-
-        Date date2 = new Date();
-        System.out.println("增加索引耗时：" + (date2.getTime() - date1.getTime()) + "ms\n");
-    }
-    
-    /**
-     * 简历索引
-     * @param id
-     * @param content
-     * @param type 'project' or 'service' or 'user'
-     * @throws Exception
-     */
-    @SuppressWarnings("deprecation")
-	public static void index(String id,String content,String type) throws Exception {
-        
-//        String text1 = "hello,man!";
-//        String text2 = "goodbye,man!";
-//        String text3 = "hello,woman!";
-//        String text4 = "goodbye,woman!";
-        
-        Date date1 = new Date();
-        analyzer = new IKAnalyzer();
-        directory = FSDirectory.open(new File(INDEX_DIR + File.separator+ type));
-
-        IndexWriterConfig config = new IndexWriterConfig(
-                Version.LUCENE_CURRENT, analyzer);
-        indexWriter = new IndexWriter(directory, config);
-
-        Document doc = new Document();
-        doc.add(new TextField("id", id, Store.YES));
-        doc.add(new TextField("content", content, Store.YES));
-        indexWriter.addDocument(doc);
-        
-        indexWriter.commit();
-        indexWriter.close();
-
-        Date date2 = new Date();
-        System.out.println("创建索引耗时：" + (date2.getTime() - date1.getTime()) + "ms\n");
-    }
+    }    
 
     /**
      * 关键字查询
+     * 返回docId List
      * 
-     * @param str
-     * @param type 'project' or 'service' or 'user'
-     * @throws Exception
+     * @param str: keyWord
+     * @param type: 索引库名字
      */
     @SuppressWarnings("deprecation")
 	public static List<String> search(String str,String type) throws Exception {
@@ -216,12 +190,73 @@ public class LuceneUtil2 {
         List<String> res = new ArrayList<String>();
         for (int i = 0; i < hits.length; i++) {
             Document hitDoc = isearcher.doc(hits[i].doc);
-            res.add(hitDoc.get("id"));
-            System.out.println(hitDoc.get("id"));
-            System.out.println(hitDoc.get("content"));
+            res.add(hitDoc.get("docId"));
+            System.out.println("searchResult: id:" + hitDoc.get("id") + " docId:"+ hitDoc.get("docId") + " content:" + hitDoc.get("content"));
         }
         ireader.close();
         directory.close();
         return res;
     }
+    
+    /**
+	     * 根据docId查询idList
+	     *  返回idList
+     * 
+     * @param docId: 
+     * @param type: 索引库名字
+     */
+    public static List<String> getIdListForDoc(Integer docId,String type) throws Exception {
+        
+    	directory = FSDirectory.open(new File(INDEX_DIR + File.separator +type));
+        analyzer = new IKAnalyzer();
+        DirectoryReader ireader = DirectoryReader.open(directory);
+        IndexSearcher isearcher = new IndexSearcher(ireader);
+
+        Query query = NumericRangeQuery.newIntRange("docId", docId,docId, true,true);// 没问题
+
+        ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+        List<String> res = new ArrayList<String>();
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = isearcher.doc(hits[i].doc);
+            res.add(hitDoc.get("id"));
+            System.out.println("searchResult: id:" + hitDoc.get("id") + " docId:"+ hitDoc.get("docId") + " content:" + hitDoc.get("content"));
+        }
+        ireader.close();
+        directory.close();
+        return res;
+    }
+
+	public static void deleteDoc(Integer docId, String type) throws Exception {
+		
+		List<String> res = getIdListForDoc(docId, type);
+		for(int i=0;i < res.size(); i++)
+		{
+			deleteIndex(res.get(i),type);
+		}
+	}
+
+	public static void addIndexForDoc(Integer docId, String filePath, String type) throws Exception {
+		addIndex(docId + "-1",docId,readFile(filePath),type);
+	}
+	
+	public static void readToBuffer(StringBuffer buffer, String filePath) throws Exception
+	{
+		InputStream is = new FileInputStream(filePath);
+		String line; // 用来保存每行读取的内容
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		line = reader.readLine(); // 读取第一行
+		while (line != null) { // 如果 line 为空说明读完了
+			buffer.append(line); // 将读到的内容添加到 buffer 中
+			buffer.append("\n"); // 添加换行符
+			line = reader.readLine(); // 读取下一行
+	    }
+	    reader.close();
+	    is.close();
+	}
+	
+	public static String readFile(String filePath) throws Exception {
+	    StringBuffer sb = new StringBuffer();
+	    readToBuffer(sb, filePath);
+	    return sb.toString();
+	}
 }
