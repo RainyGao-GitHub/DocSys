857611aac3062e6826340086cb93cd084e0ea99b
RainyGao <652055239@qq.com>
2018-11-04 22:37:43 +0800

    全文搜索实现: Delete Index Failed If Use VDoec-1-0 and RDoc-1-0 as the id

--- src/LuceneTest.java
+++ src/LuceneTest.java
@@ -7,11 +7,11 @@ class LuceneTest
     public static void main(String[] args)    
     {  
         System.out.println("This is test app");
-        String file1Content1 = "abc efg hijk lmn ccccccccccccccccccccccc 国家";
+        String content = "abc efg hijk lmn ccccccccccccccccccccccc 国家";
        // String file1Content2 = "abc hijk efg ddddd 中国";
         try {
         	System.out.println("************* Add Index Test ****************");
-        	LuceneUtil2.addIndexForVDoc(1,file1Content1,"doc");        	
+        	//LuceneUtil2.addIndexForVDoc(1,content,"doc");        	
         	LuceneUtil2.addIndexForRDoc(1,"C:/Users/Administrator/Desktop/77777ddd.txt","doc");
         	
         	System.out.println("************* Search Test ****************");
