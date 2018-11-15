571db3601185faae681c122c4b068704b3759b5c
Rainy <652055239@qq.com>
2018-11-15 14:48:30 +0800

    全文搜索实现: use cpdetector to get the file Encode Type

--- src/LuceneTest.java
+++ src/LuceneTest.java
@@ -11,20 +11,23 @@ class LuceneTest
        // String file1Content2 = "abc hijk efg ddddd 中国";
         try {
         	System.out.println("************* Add Index Test ****************");
-        	LuceneUtil2.addIndexForVDoc(1,content,"doc");        	
+        	//LuceneUtil2.addIndexForVDoc(1,content,"doc");        	
         	//LuceneUtil2.addIndexForRDoc(1,"C:/Users/Administrator/Desktop/77777ddd.txt","doc");
         	//LuceneUtil2.addIndexForRDoc(1,"C:/Users/Administrator/Desktop/CommitFailTest.xlsx","doc");
-        	LuceneUtil2.addIndexForRDoc(1,"C:/Users/Administrator/Desktop/getHistoryDoc.js","doc");
-
+        	//LuceneUtil2.addIndexForRDoc(1,"C:/Users/Administrator/Desktop/getHistoryDoc.js","doc");
+        	//LuceneUtil2.addIndexForRDoc(1,"C:\\Users\\ragao\\Desktop\\Rainy\\DocSys仓库详情页面布局需求.docx","doc");
+        	//LuceneUtil2.addIndexForRDoc(1,"C:\\Users\\ragao\\Desktop\\Rainy\\ci_dspnyq.log","doc");
+        	LuceneUtil2.addIndexForRDoc(1,"C:\\Users\\ragao\\Desktop\\Rainy\\URLProtocol.html","doc");
+        	
         	System.out.println("************* Search Test ****************");
-        	LuceneUtil2.fuzzySearch("7788","doc");
+        	LuceneUtil2.fuzzySearch("国家","doc");
         	
         	System.out.println("*********** Delete Index Test *********** ");
            	//LuceneUtil2.deleteIndexForDoc(1,"doc");
            	LuceneUtil2.deleteIndexForVDoc(1,"doc");
            	LuceneUtil2.deleteIndexForRDoc(1,"doc");
            	System.out.println("************* Search Test after delete****************");
-        	LuceneUtil2.fuzzySearch("7788","doc");
+        	LuceneUtil2.fuzzySearch("国家","doc");
         } catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
