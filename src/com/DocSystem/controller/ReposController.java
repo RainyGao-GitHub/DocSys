5b37a5d66b439435951468dddc791281390611f5
Rainy <652055239@qq.com>
2019-05-04 10:23:22 +0800

    SuperAdmin should always can access the manager menu

--- src/com/DocSystem/controller/ReposController.java
+++ src/com/DocSystem/controller/ReposController.java
@@ -610,7 +610,18 @@ public class ReposController extends BaseController{
 		//获取用户可访问文件列表(From Root to docId)
 		
 		//get the rootDocAuth
-		DocAuth rootDocAuth = getUserDispDocAuth(repos, login_user.getId(), 0, "", "");
+		DocAuth rootDocAuth = null;
+		if(login_user.getType() == 2)	//超级管理员可以访问所有目录
+		{
+			rootDocAuth = new DocAuth();
+			rootDocAuth.setAccess(1);
+			System.out.println("超级管理员");
+		}
+		else
+		{
+			rootDocAuth = getUserDispDocAuth(repos, login_user.getId(), 0, "", "");
+		}
+		
 		if(rootDocAuth == null || rootDocAuth.getAccess() == null || rootDocAuth.getAccess() == 0)
 		{
 			System.out.println("getReposManagerMenu() 您没有该仓库的访问权限，请联系管理员！");
