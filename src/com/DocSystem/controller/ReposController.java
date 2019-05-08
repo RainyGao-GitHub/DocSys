13e30448c1b598567922f62029188a3ad241aeff
Rainy <652055239@qq.com>
2019-05-08 14:58:51 +0800

    clean code

--- src/com/DocSystem/controller/ReposController.java
+++ src/com/DocSystem/controller/ReposController.java
@@ -1,6 +1,5 @@
 package com.DocSystem.controller;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -14,10 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import util.ReturnAjax;
-import util.GitUtil.GITUtil;
 import util.LuceneUtil.LuceneUtil2;
-import util.SvnUtil.SVNUtil;
-
 import com.DocSystem.entity.DocAuth;
 import com.DocSystem.entity.Repos;
 import com.DocSystem.entity.Doc;
