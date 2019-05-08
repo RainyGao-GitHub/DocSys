93dc1910786599c9587ba32c68b84b3657f5503a
Rainy <652055239@qq.com>
2019-05-08 15:03:49 +0800

    Change docId to long, so that it always be positive

--- src/com/DocSystem/common/BaseFunction.java
+++ src/com/DocSystem/common/BaseFunction.java
@@ -362,7 +362,7 @@ public class BaseFunction{
 	//Build DocId by DocName
 	protected Long buildDocIdByName(Integer level, String docName) 
 	{
-		Long docId = (long) (level*100000000 + docName.hashCode());	//为了避免文件重复使用level*100000000 + docName的hashCode
+		Long docId = (long) level*1000000000 + (long) docName.hashCode();	//为了避免文件重复使用level*100000000 + docName的hashCode
 		return docId;
 	}
 	
