5381f560b37e45baf8eaf7b8cff4c58b14e9d1d4
Rainy <652055239@qq.com>
2019-07-08 16:48:49 +0800

    Fix download bug

--- WebRoot/web/js/DocDownload.js
+++ WebRoot/web/js/DocDownload.js
@@ -267,7 +267,7 @@
                 	    console.log("downloadDocPrepare Ok:",ret);
             	   		var encPath = encodeURI(SubContext.path);
             	   		var encName = encodeURI(SubContext.name);
-            	   		var downloadType = ret.data.msgData;
+            	   		var downloadType = ret.msgData;
             	   		window.location.href = "/DocSystem/Doc/downloadDoc.do?reposId=" + SubContext.vid + "&docId=" + SubContext.docId + "&pid=" + SubContext.pid + "&path=" + encPath + "&name="+ encName  + "&downloadType="+ downloadType;	
 
                 	   	downloadSuccessHandler(SubContext, ret.msgInfo);
