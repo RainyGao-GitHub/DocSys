56c6ecc1879b0b5fc3e11ab30c744d6cff567434
Rainy <652055239@qq.com>
2019-07-18 09:25:52 +0800

    Change the Filed to dedicated type

--- src/util/LuceneUtil/LuceneUtil2.java
+++ src/util/LuceneUtil/LuceneUtil2.java
@@ -203,10 +203,10 @@ public class LuceneUtil2   extends BaseFunction
 
 	private static Document buildDocument(Doc doc, String content) {
 		Document document = new Document();
-        document.add(new TextField("vid", doc.getVid()+"", Store.YES));
-        document.add(new TextField("pid", doc.getPid()+"", Store.YES));	//docId总是可以通过docPath 和 docName计算出来
-        document.add(new TextField("docId", doc.getDocId()+"", Store.YES));	//docId总是可以通过docPath 和 docName计算出来
-        document.add(new TextField("type", doc.getType()+"", Store.YES));	//1: file 2: dir 用来保存Lucene和实际文件的区别
+        document.add(new IntField("vid", doc.getVid(), Store.YES));
+        document.add(new LongField("pid", doc.getPid(), Store.YES));	//docId总是可以通过docPath 和 docName计算出来
+        document.add(new LongField("docId", doc.getDocId(), Store.YES));	//docId总是可以通过docPath 和 docName计算出来
+        document.add(new IntField("type", doc.getType(), Store.YES));	//1: file 2: dir 用来保存Lucene和实际文件的区别
         document.add(new Field("path", doc.getPath(), Store.YES, Index.NOT_ANALYZED_NO_NORMS));	
         document.add(new Field("name", doc.getName(), Store.YES, Index.NOT_ANALYZED_NO_NORMS));	//文件名需要用于通配符搜索，因此不能进行切词处理
         document.add(new TextField("content", content, Store.NO));	//Content有可能会很大，所以只切词不保存	        
