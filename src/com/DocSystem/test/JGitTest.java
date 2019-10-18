8e803fe8cbeaef53fdeb7856af406fcbeae6cae3
Rainy <652055239@qq.com>
2019-10-18 18:11:41 +0800

    pullEx和push测试

--- src/com/DocSystem/test/JGitTest.java
+++ src/com/DocSystem/test/JGitTest.java
@@ -46,36 +46,37 @@ class JGitTest extends ReposController{
     	localRepos.setVerCtrl(2);
     	localRepos.setIsRemote(1);
     	localRepos.setLocalSvnPath("C:/JGitTestDir/LocalGitRepos/");
-    	localRepos.setSvnPath("C:/JGitTestDir/OriginGitRepos/123456/");
+    	localRepos.setSvnPath("C:/JGitTestDir/OriginGitRepos/123456_GIT_RRepos");
     	localRepos.setId(234567);
+    	localRepos.setSvnUser(null);
     	
     	//Create realDocPath
-    	File localDir = new File(repos.getRealDocPath());
-    	if(!localDir.exists())
+    	File originDocRootDir = new File(repos.getRealDocPath());
+    	if(!originDocRootDir.exists())
     	{
-    		localDir.mkdirs();
+    		originDocRootDir.mkdirs();
     	}
-    	localDir = new File(localRepos.getRealDocPath());
-    	if(!localDir.exists())
+    	File localDocRootDir = new File(localRepos.getRealDocPath());
+    	if(!localDocRootDir.exists())
     	{
-    		localDir.mkdirs();
+    		localDocRootDir.mkdirs();
     	}
     	
     	//Create Git Repos
-    	GITUtil gitUtil = new GITUtil();
-    	gitUtil.Init(repos, isRealDoc, "");
-    	String gitPath = gitUtil.CreateRepos();
-    	System.out.println("gitPath:" + gitPath);
-    	if(gitPath == null)
+    	GITUtil originGitUtil = new GITUtil();
+    	originGitUtil.Init(repos, isRealDoc, "");
+    	String originGitPath = originGitUtil.CreateRepos();
+    	System.out.println("originGitPath:" + originGitPath);
+    	if(originGitPath == null)
     	{
     		System.out.println("createGitLocalRepos Failed!");
     		return;
     	}
     	
     	//Clone
-		GITUtil gitUtil1 = new GITUtil();
-		gitUtil1.Init(repos, isRealDoc, "");
-        gitUtil1.CloneRepos();
+		GITUtil localGitUtil = new GITUtil();
+		localGitUtil.Init(localRepos, isRealDoc, "");
+		localGitUtil.CloneRepos();
         
     	//Auto Commit to Origin Git Repos at root dir
     	Doc doc = new Doc();
@@ -84,7 +85,7 @@ class JGitTest extends ReposController{
     	doc.setPath("");
     	doc.setName("");
     	doc.setLocalRootPath(repos.getRealDocPath());
-		if(null == gitUtil.doAutoCommit(doc, "Init GitRepos", "RainyGao",true,null,2))
+		if(null == originGitUtil.doAutoCommit(doc, "Init OriginGitRepos", "OriginRainyGao",true,null,2))
 		{
     		System.out.println("gitAutoCommit Failed!");
     		return;
@@ -92,12 +93,24 @@ class JGitTest extends ReposController{
 		
     	//Auto Commit to Local Git Repos at root dir
     	doc.setLocalRootPath(localRepos.getRealDocPath());
-		if(null == gitUtil1.doAutoCommit(doc, "Init GitRepos", "RainyGao",true,null,2))
+		if(null == localGitUtil.doAutoCommit(doc, "Init LocalGitRepos", "LocalRainyGao",true,null,2))
 		{
     		System.out.println("gitAutoCommit Failed!");
     		return;
 		}
 		
 		//Do rebase 
+		if(localGitUtil.doPullEx() == false)
+		{
+    		System.out.println("gitAutoCommit doPullEx Failed!");
+    		return;			
+		}
+		
+		//Do push
+		if(localGitUtil.doPush() == false)
+		{
+    		System.out.println("gitAutoCommit doPush Failed!");
+    		return;		
+		}
     }
 } 
