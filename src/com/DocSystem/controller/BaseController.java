b0ef0490e59fb74677cbf1e7b0e51fdece47ade4
Rainy <652055239@qq.com>
2019-12-03 15:12:39 +0800

    Fix the bug

--- src/com/DocSystem/controller/BaseController.java
+++ src/com/DocSystem/controller/BaseController.java
@@ -60,7 +60,7 @@ import util.SvnUtil.SVNUtil;
 
 public class BaseController  extends BaseFunction{
 	@Autowired
-	protected static ReposServiceImpl reposService;
+	protected ReposServiceImpl reposService;
 	@Autowired
 	protected UserServiceImpl userService;
 	
