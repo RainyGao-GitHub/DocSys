38218045c03cdcdaeba690024e1146aa77584a37
Rainy <652055239@qq.com>
2019-12-14 23:04:39 +0800

    fix bug

--- src/com/DocSystem/test/ExportDocAuthList.java
+++ src/com/DocSystem/test/ExportDocAuthList.java
@@ -4,6 +4,7 @@ import com.DocSystem.controller.BaseController;
 
 class ExportDocAuthList extends BaseController{
     public static void main(String[] args) {
-    	exportObjectListToJsonFile(DOCSYS_DOC_AUTH, "docAuthList.json", 20000, 20000);
+    	exportObjectListToJsonFile(DOCSYS_USER, "", "docAuthList.json", 0, 20000);
+        //exportObjectListToJsonFile(DOCSYS_DOC_AUTH, "", "docAuthList.json", 0, 20000);
     }
 }
