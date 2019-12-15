7fcafc159de5130ac72b62a2426fcdbc821e176d
Rainy <652055239@qq.com>
2019-12-15 10:38:45 +0800

    getFileEncode接口修改

--- src/com/DocSystem/test/DocSysInitTest.java
+++ src/com/DocSystem/test/DocSysInitTest.java
@@ -4,8 +4,13 @@ import com.DocSystem.controller.BaseController;
 
 class DocSysInitTest extends BaseController{
     public static void main(String[] args) {
-    	//backupDB Test
+    	//backupDB as sql file Test
     	backupDB("/DocSysTestDir/", "docsystem.sql", "UTF-8");
     	
+    	//Export to json file Test
+    	exportObjectListToJsonFile(DOCSYS_USER, "", "docAuthList.json", 0, 20000);
+    	
+    	//import json to DB Test
+    	importObjectListFromJsonFile(DOCSYS_DOC_AUTH, "", "docAuthList.json");
     }
 }
--- src/com/DocSystem/test/cpdetectorTest.java
+++ src/com/DocSystem/test/cpdetectorTest.java
@@ -1,12 +1,8 @@
 package com.DocSystem.test;
-import java.io.File;
-import info.monitorenter.cpdetector.io.ASCIIDetector;
-import info.monitorenter.cpdetector.io.CodepageDetectorProxy;
-import info.monitorenter.cpdetector.io.JChardetFacade;
-import info.monitorenter.cpdetector.io.ParsingDetector;
-import info.monitorenter.cpdetector.io.UnicodeDetector;
 
-class cpdetectorTest  
+import com.DocSystem.controller.BaseController;
+
+class cpdetectorTest  extends BaseController
 {  
     public static void main(String[] args)    
     {  
@@ -23,26 +19,5 @@ class cpdetectorTest
         FileEncode = getFileEncode("C:\\Users\\ragao\\Desktop\\Rainy\\Print\\0921\\1105698019.jpg");       
         System.out.println("FileEncode:" + FileEncode); 
         
-    }
-    
-    public static String getFileEncode(String filePath) {
-        String charsetName = null;
-        try {
-            File file = new File(filePath);
-            CodepageDetectorProxy detector = CodepageDetectorProxy.getInstance();
-            detector.add(new ParsingDetector(false));
-            detector.add(JChardetFacade.getInstance());
-            detector.add(ASCIIDetector.getInstance());
-            detector.add(UnicodeDetector.getInstance());
-            java.nio.charset.Charset charset = null;
-            charset = detector.detectCodepage(file.toURI().toURL());
-            if (charset != null) {
-                charsetName = charset.name();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return charsetName;
     }
 }
