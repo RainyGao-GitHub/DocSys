46cc58b8c11d9c2d4ff360a0404bedf5cfa1dfd8
Rainy <652055239@qq.com>
2019-12-18 16:57:10 +0800

    docSysInit Test

--- src/com/DocSystem/test/DocSysInitTest.java
+++ src/com/DocSystem/test/DocSysInitTest.java
@@ -5,13 +5,13 @@ import com.DocSystem.controller.BaseController;
 class DocSysInitTest extends BaseController{
     public static void main(String[] args) {
     	//backupDB as sql file Test
-    	backupDB("/DocSysTestDir/", "docsystem.sql", "UTF-8");
+    	//backupDB("/DocSysTestDir/", "docsystem.sql", "UTF-8");
     	
     	//Export to json file Test
-    	exportObjectListToJsonFile(DOCSYS_USER, "/DocSysTestDir/", "USER.json", 0, 20000);
+    	//exportObjectListToJsonFile(DOCSYS_USER, "/DocSysTestDir/", "USER.json", 0, 20000);
     	
     	//Import json to DB Test
-    	importObjectListFromJsonFile(DOCSYS_USER, "/DocSysTestDir/", "USER.json");
+    	//importObjectListFromJsonFile(DOCSYS_USER, "/DocSysTestDir/", "USER.json");
     	
     	//DB upgrade Test
     	docSysWebPath = "/DocSysTestDir/WebRoot/";
