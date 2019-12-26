673c3c941a55468592af2caf6a0a97828ff2ed85
Rainy <652055239@qq.com>
2019-12-26 14:23:47 +0800

    Repos ok

--- src/com/DocSystem/common/BaseFunction.java
+++ src/com/DocSystem/common/BaseFunction.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -2496,4 +2497,30 @@ public class BaseFunction{
 		return false;
 	}
 	
+	/*根据变量名对变量进行设置*/
+	public static boolean setFieldValue(Object object,String field,Object value){
+	    boolean ret = false;
+		char[] chars = field.trim().toCharArray();
+	    chars[0] -= 32;//将field的首字母转为大写，因为set方法后跟的是首字母大写的属性
+	    try {
+	        Method method = object.getClass().getMethod("set" + String.valueOf(chars), value.getClass());
+	        method.invoke(object, value);
+	        return true;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return ret;
+	}
+	
+	public static Object getFieldValue(Object object,String field){
+	    char[] chars = field.trim().toCharArray();
+	    chars[0] -= 32;//将field的首字母转为大写，因为set方法后跟的是首字母大写的属性
+	    try {
+	        Method method = object.getClass().getMethod("get" + String.valueOf(chars));
+	        return method.invoke(object);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
 }
