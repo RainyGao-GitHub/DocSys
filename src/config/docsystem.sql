5c8bf35da0c91cf93f9361f9880514f12946eda7
Rainy <652055239@qq.com>
2019-12-27 22:00:38 +0800

    add DocShare to docsystem.sql

--- src/config/docsystem.sql
+++ src/config/docsystem.sql
@@ -74,6 +74,25 @@ CREATE TABLE `doc_auth` (
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `doc_share`
+--
+
+CREATE TABLE `doc_share` (
+  `ID` int(11) NOT NULL,
+  `SHARE_ID` int(11) NOT NULL COMMENT '分享ID',
+  `NAME` varchar(200) DEFAULT NULL COMMENT '分享的文件或目录名称',
+  `PATH` varchar(2000) NOT NULL DEFAULT '' COMMENT '基于仓库目录的相对路径',
+  `DOC_ID` bigint(20) DEFAULT NULL COMMENT 'Doc Node id',
+  `VID` int(11) DEFAULT NULL COMMENT '所属仓库id',
+  `SHARE_AUTH` varchar(2000) DEFAULT NULL COMMENT '分享权限',
+  `SHARE_PWD` varchar(20) DEFAULT NULL COMMENT '分享密码',
+  `SHARED_BY` int(11) DEFAULT NULL COMMENT '分享用户ID',
+  `EXPIRE_TIME` bigint(20) NOT NULL DEFAULT '0' COMMENT '分享有效时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `doc_lock`
 --
 
@@ -248,6 +267,13 @@ ALTER TABLE `doc_auth`
 ALTER TABLE `doc_lock`
   ADD PRIMARY KEY (`ID`);
 
+  --
+-- Indexes for table `doc_share`
+--
+ALTER TABLE `doc_share`
+  ADD PRIMARY KEY (`ID`);
+
+
 --
 -- Indexes for table `group_member`
 --
@@ -313,6 +339,12 @@ ALTER TABLE `doc_auth`
 ALTER TABLE `doc_lock`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
+ --
+-- 使用表AUTO_INCREMENT `doc_share`
+--
+ALTER TABLE `doc_share`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  
 --
 -- 使用表AUTO_INCREMENT `group_member`
 --
