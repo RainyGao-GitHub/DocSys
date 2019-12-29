345d86b222765b2dd485c079b0e6e9b6e414a828
Rainy <652055239@qq.com>
2019-12-29 21:04:47 +0800

    Fix bug for docId is null

--- src/com/DocSystem/test/DocSysInitTest.java
+++ src/com/DocSystem/test/DocSysInitTest.java
@@ -11,16 +11,18 @@ class DocSysInitTest extends BaseController{
     	//backupDB as sql file Test
     	//backupDB("/DocSysTestDir/", "docsystem.sql", "UTF-8");
     	
-    	//数据库导入测试
+    	//数据库脚本执行测试
     	//String filePath = docSysWebPath + "docsystem_DOC_SHARE.sql";
     	//executeSqlScript(filePath);
     	
     	//数据库导出测试
     	//initObjMemberListMap();
     	//exportObjectListToJsonFile(DOCSYS_REPOS, "/DocSysTestDir/", "REPOS.json", 0, 20000);
+    	//exportObjectListToJsonFile(DOCSYS_DOC_AUTH, "/DocSysTestDir/", "DOC_AUTH.json", 0, 20000);
     	
     	//数据库导入测试
     	//importObjectListFromJsonFile(DOCSYS_REPOS, "/DocSysTestDir/", "REPOS.json");
+    	//importObjectListFromJsonFile(DOCSYS_DOC_AUTH, "/DocSysTestDir/", "DOC_AUTH.json");
     	
     	//系统初始化测试
     	docSysInit();
