0a940df2f0a4e965dac01832ae177eb48dbd5f4d
Rainy <652055239@qq.com>
2019-12-29 10:28:16 +0800

    add log

--- src/com/DocSystem/test/DocSysInitTest.java
+++ src/com/DocSystem/test/DocSysInitTest.java
@@ -16,11 +16,11 @@ class DocSysInitTest extends BaseController{
     	//executeSqlScript(filePath);
     	
     	//数据库导出测试
-    	initObjMemberListMap();
-    	exportObjectListToJsonFile(DOCSYS_REPOS, "/DocSysTestDir/", "REPOS.json", 0, 20000);
+    	//initObjMemberListMap();
+    	//exportObjectListToJsonFile(DOCSYS_REPOS, "/DocSysTestDir/", "REPOS.json", 0, 20000);
     	
     	//数据库导入测试
-    	importObjectListFromJsonFile(DOCSYS_REPOS, "/DocSysTestDir/", "REPOS.json");
+    	//importObjectListFromJsonFile(DOCSYS_REPOS, "/DocSysTestDir/", "REPOS.json");
     	
     	//系统初始化测试
     	docSysInit();
