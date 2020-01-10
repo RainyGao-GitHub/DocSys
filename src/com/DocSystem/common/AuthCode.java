20da5c8f3a457bcf2cb62197beb8897451e009ff
Rainy <652055239@qq.com>
2020-01-10 13:09:24 +0800

    add AuthCode

---
+++ src/com/DocSystem/common/AuthCode.java
@@ -0,0 +1,44 @@
+package com.DocSystem.common;
+
+public class AuthCode {
+	private String code = null; //hashCode for authCode
+	private String usage = null;	//用途
+    private Long expTime = null;	//超时时间戳
+	private Integer remainCount = 1; //默认只能使用一次
+	
+	public String getCode()
+	{
+		return code;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getUsage()
+	{
+		return usage;
+	}
+	
+	public void setUsage(String usage) {
+		this.usage = usage;
+	}
+	
+	public Long getExpTime()
+	{
+		return expTime;
+	}
+	
+	public void setExpTime(Long expTime) {
+		this.expTime = expTime;
+	}
+	
+	public Integer getRemainCount()
+	{
+		return remainCount;
+	}
+	
+	public void setRemainCount(Integer remainCount) {
+		this.remainCount = remainCount;
+	}
+}
