7947b318b1f55b11e2bfe93942e2b1112d410894
yuan <13777479349@139.com>
2020-03-16 17:40:21 +0800

    分享服务器只允许与远程代理服务器建立一个连接

--- src/com/DocSystem/commonService/ShareThread.java
+++ src/com/DocSystem/commonService/ShareThread.java
@@ -1,13 +1,71 @@
 package com.DocSystem.commonService;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ShareThread extends Thread {
+
+	boolean stopShareThread = false;
+	String proxyServerIP = null;
+	public void setProxySererIP(String proxyServerIP)
+    {
+        this.proxyServerIP = proxyServerIP;
+    }
+	
 	public void run() 
 	{
 		String name = Thread.currentThread().getName();
 		String inf=Thread.currentThread().toString();
 		long idnum = Thread.currentThread().getId();
-		for(int i=0;i<10;i++){//不管是新建一个对象，还是两个对象，//2，都是打印20个数据
-			// for(;i<10;i++){//新建一个对象的时候，打印11个左右的数据 ,新建两个对象的时候，//2，会打印20个数据。//1
-			System.out.println("i----------"+i +",thread name=="+ name +",threadid=="+ idnum+",thread inf=="+inf);
+		System.out.println("ShareThread: thread name=="+ name +",threadid=="+ idnum+",thread inf=="+inf);
+
+		int port = 55533;
+		if(proxyServerIP != null)
+		{
+			Socket socket;
+			try {
+				socket = new Socket(proxyServerIP, port);
+				sendShareRequest(socket);
+				if(receiveShareRespose(socket) == false)
+				{
+					socket.close();
+					return;
+				}
+				
+				while(stopShareThread == false)
+				{
+					//msg = receive();
+					//msgHandler();
+				}
+				socket.close();				
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+	}
+
+	private boolean receiveShareRespose(Socket socket) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private void sendShareRequest(Socket socket) {
+		OutputStream outputStream;
+		try {
+			outputStream = socket.getOutputStream();
+			String message="你好  yiwangzhibujian";
+			socket.getOutputStream().write(message.getBytes("UTF-8"));
+			outputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	
 	}
 }
