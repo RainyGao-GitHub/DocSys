61b5bc1b43168be1ecaf80d712903a6bd245e01a
yuan <13777479349@139.com>
2020-04-17 11:20:35 +0800

    add interface for getDocumentType

---
+++ WebRoot/web/js/common.js
@@ -0,0 +1,12 @@
+/*这里定义了通用的函数接口（与业务、页面无依赖）*/
+
+//从 url 中获取参数
+function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
+
