6c0d99dfa448cef0044ff02acfeb39d583fe1831
yuan <13777479349@139.com>
2020-05-08 09:59:52 +0800

    add more banner

--- src/com/DocSystem/controller/ManageController.java
+++ src/com/DocSystem/controller/ManageController.java
@@ -2,7 +2,6 @@ package com.DocSystem.controller;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
