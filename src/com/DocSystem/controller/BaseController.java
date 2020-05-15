f2960f6cee797a3825b08c2a216447dc216cf9d7
yuan <13777479349@139.com>
2020-05-15 22:23:17 +0800

    同时删除大小写的仓库

--- src/com/DocSystem/controller/BaseController.java
+++ src/com/DocSystem/controller/BaseController.java
@@ -8449,6 +8449,19 @@ public class BaseController  extends BaseFunction{
 		}	
 		return true;
 	}
+	
+	//删除数据库表（包括小写的）
+	protected static boolean deleteDBTabsEx(String type, String url, String user, String pwd) 
+	{
+		System.out.println("deleteDBTabs()");
+
+		for(int i=0; i< DBTabNameMap.length; i++)
+		{
+			deleteDBTab(DBTabNameMap[i].toUpperCase(), type, url, user, pwd);
+			deleteDBTab(DBTabNameMap[i].toLowerCase(), type, url, user, pwd); //删除小写的数据库			
+		}	
+		return true;
+	}
 
 	protected static boolean initDB(String type, String url, String user, String pwd) 
 	{
