4d1ec5376a6ef801f6cfb9b03e244139520700a2
yuan <13777479349@139.com>
2020-06-30 15:31:39 +0800

    将stop和start拆分成两个脚本，避免stop失败无法执行start,但似乎还不是不能工作

---
+++ src/script/tomcat_start.bat
@@ -0,0 +1,13 @@
+echo.
+echo [DocSys]: Start
+set "CATALINA_HOME=tomcatPath"
+set "JAVA_HOME=javaHome"
+set "JRE_HOME=javaHome"
+
+echo [DocSys]: Start Tomcat
+"%CATALINA_HOME%\bin\catalina.bat" run
+
+echo done.
+pause
+
+
