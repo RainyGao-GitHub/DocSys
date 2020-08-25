601e8f8d5d33a1dbfdb4b87eba302c6cba9347a8
rainy <652055239@qq.com>
2020-08-25 22:27:43 +0800

    add OfficeEditorForArt

--- WebRoot/web/js/DocSys.js
+++ WebRoot/web/js/DocSys.js
@@ -1407,7 +1407,14 @@ function showOffice(docInfo, openInNewPage)
 	}
 	else
 	{
-		showOfficeInDialog(docInfo);
+		if(gIsPC)
+		{			
+			showOfficeInArtDialog(docInfo);
+		}
+		else
+		{
+			showOfficeInDialog(docInfo);
+		}
 	}
 }
 
@@ -1906,6 +1913,62 @@ function showOfficeInDialog(docInfo)
 			setTimeout(function (){OfficeEditor.officeEditorPageInit(docInfo)}, 2000); 
 		},
 	});
+}
+
+function showOfficeInArtDialog(docInfo)
+{	
+	//获取窗口的高度并设置高度
+	var height =  window.screen.height/2;
+	var width = window.screen.width/2;	
+	var d = dialog({
+		id: "OfficeEditor"  + docInfo.docId,
+		title: docInfo.name,
+		url: 'officeEditorForArt.jsp',
+		msg: '页面正在加载，请稍等...',
+		foot: false,
+		big: true,
+		padding: 0,
+		width: width,
+		height: height,
+		resize: true,
+		drag: true,
+		data: docInfo,
+		onshow: function(){
+			console.log('onshow');
+		},
+		oniframeload: function () {
+			console.log('oniframeload');
+		},
+	});
+	d.show();
+
+	//等待页面加载好了再获取
+	setTimeout(function (){
+		var isMax = false;
+		
+		var oDiv = document.getElementById("title:OfficeEditor"  + docInfo.docId);
+		oDiv.ondblclick=function(ev){
+	    	console.log("DB Clicked on " +"OfficeEditor"  + docInfo.docId);
+			if(isMax)
+			{
+				var height =  window.screen.height/2;
+				var width = window.screen.width/2;
+				
+				isMax = false;
+				d.width(width);
+				d.height(height);	
+			}
+			else
+			{
+				//最大化
+				var height =  getWinHeight()-50;
+				var width = getWinWidth();
+				isMax = true;
+				d.width(width);
+				d.height(height);		
+			}	
+		}
+	}, 100);
 }
 
 function getZipDialogStyle()
