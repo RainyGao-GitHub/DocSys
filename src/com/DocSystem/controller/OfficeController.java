5d487b0d1e208ea3695fc7e949ce5a3571a2e919
yuan <13777479349@139.com>
2020-11-18 21:11:53 +0800

    Remove websocket code

--- src/com/DocSystem/controller/OfficeController.java
+++ src/com/DocSystem/controller/OfficeController.java
@@ -11,11 +11,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 @Controller
-@RequestMapping("/web/static/office-editor/doc")
+@RequestMapping("/web/static/office-editor")
 public class OfficeController extends BaseController{
 	// get请求
-	@RequestMapping(value="/{key}/c/info", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
-	//@RequestMapping(value="/{key}/c/info", method=RequestMethod.GET)
+	@RequestMapping(value="/doc/{key}/c/info", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
 	public void getInfo(@PathVariable("key") String key, HttpServletResponse response) {
 		
 		System.out.println("getInfo key:" + key);
@@ -28,7 +27,58 @@ public class OfficeController extends BaseController{
 		//{"websocket":true,"origins":["*:*"],"cookie_needed":false,"entropy":3999383772} //前端期望的返回		
     }
 	
-//	// post请求
+	// get请求
+	@RequestMapping(value="/spellchecker/doc/{key}/c/info", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
+	public void getSpellCheckerInfo(@PathVariable("key") String key, HttpServletResponse response) {
+		
+		System.out.println("getSpellCheckerInfo key:" + key);
+		JSONObject jobj = new JSONObject();
+		jobj.put("websocket", true);
+		jobj.put("origins", "*:*");
+		jobj.put("cookie_needed", false);
+		jobj.put("entropy", "2782586626");
+		writeJson(jobj, response);
+		//{"websocket":true,"origins":["*:*"],"cookie_needed":false,"entropy":3999383772} //前端期望的返回		
+    }
+	
+	// get请求
+	@RequestMapping(value="/{key}/c/{val1}/{val2}/jsonp", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
+	public void jsonp(@PathVariable("key") String key, HttpServletResponse response) {
+		
+		System.out.println("jsonp key:" + key);
+		JSONObject jobj = new JSONObject();
+		jobj.put("websocket", true);
+		jobj.put("origins", "*:*");
+		jobj.put("cookie_needed", false);
+		jobj.put("entropy", "2782586626");
+		writeJson(jobj, response);
+    }
+	
+	// get请求
+	@RequestMapping(value="/{key}/c/iframe.html", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
+	public void getIframe(@PathVariable("key") String key, HttpServletResponse response) {
+		
+		System.out.println("getIframe key:" + key);
+		JSONObject jobj = new JSONObject();
+		jobj.put("websocket", true);
+		jobj.put("origins", "*:*");
+		jobj.put("cookie_needed", false);
+		jobj.put("entropy", "2782586626");
+		writeJson(jobj, response);
+    }
+
+    // post请求
+	@RequestMapping(value="/{key}/c/{val1}/{val2}/xhr", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
+	public void xhr(@PathVariable("key") String key, HttpServletResponse response) {
+		System.out.println("xhr key:" + key);
+		JSONObject jobj = new JSONObject();
+		jobj.put("websocket", true);
+		jobj.put("origins", "*:*");
+		jobj.put("cookie_needed", false);
+		jobj.put("entropy", "2782586626");
+		writeJson(jobj, response);
+    }
+	
 //	@RequestMapping(value="/restfulUser", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
 //	public String postUser(String name, Integer age) {
 //	    return "接收到POST："+name+" "+age;
