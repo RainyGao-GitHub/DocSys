8a9e742cc253346b44a2b8a5cfdfbc55ba697249
yuan <13777479349@139.com>
2021-02-22 14:54:01 +0800

    优化代码

---
+++ src/com/DocSystem/common/constants.java
@@ -0,0 +1,160 @@
+package com.DocSystem.common;
+
+//This Object was designed for DocData which is doc unique
+public class constants {
+	public final static int AVS_OFFICESTUDIO_FILE_UNKNOWN =  0x0000;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT = 0x0040;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_DOCX = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_DOC = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_ODT = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0003;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_RTF = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0004;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_TXT = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0005;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_HTML = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0006;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_MHT = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0007;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_EPUB = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0008;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_FB2 = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0009;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_MOBI = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x000a;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_DOCM = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x000b;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_DOTX = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x000c;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_DOTM = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x000d;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_ODT_FLAT = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x000e;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_OTT = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x000f;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_DOC_FLAT = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0010;
+	public final static int AVS_OFFICESTUDIO_FILE_DOCUMENT_HTML_IN_CONTAINER = AVS_OFFICESTUDIO_FILE_DOCUMENT + 0x0011;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION = 0x0080;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_PPTX = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_PPT = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_ODP = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0003;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_PPSX = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0004;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_PPTM = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0005;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_PPSM = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0006;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_POTX = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0007;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_POTM = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0008;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_ODP_FLAT = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x0009;
+	public final static int AVS_OFFICESTUDIO_FILE_PRESENTATION_OTP = AVS_OFFICESTUDIO_FILE_PRESENTATION + 0x000a;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET = 0x0100;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_XLSX = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_XLS = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_ODS = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0003;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_CSV = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0004;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_XLSM = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0005;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_XLTX = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0006;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_XLTM = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0007;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_ODS_FLAT = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0008;
+	public final static int AVS_OFFICESTUDIO_FILE_SPREADSHEET_OTS = AVS_OFFICESTUDIO_FILE_SPREADSHEET + 0x0009;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM = 0x0200;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_PDF = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_SWF = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_DJVU = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0003;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_XPS = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0004;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_SVG = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0005;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_HTMLR = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0006;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_HTMLR_MENU = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0007;
+	public final static int AVS_OFFICESTUDIO_FILE_CROSSPLATFORM_HTMLR_CANVAS = AVS_OFFICESTUDIO_FILE_CROSSPLATFORM + 0x0008;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE = 0x0400;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_JPG = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_TIFF = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_TGA = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0003;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_GIF = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0004;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_PNG = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0005;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_EMF = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0006;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_WMF = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0007;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_BMP = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0008;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_CR2 = AVS_OFFICESTUDIO_FILE_IMAGE + 0x0009;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_PCX = AVS_OFFICESTUDIO_FILE_IMAGE + 0x000a;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_RAS = AVS_OFFICESTUDIO_FILE_IMAGE + 0x000b;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_PSD = AVS_OFFICESTUDIO_FILE_IMAGE + 0x000c;
+	public final static int AVS_OFFICESTUDIO_FILE_IMAGE_ICO = AVS_OFFICESTUDIO_FILE_IMAGE + 0x000d;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER = 0x0800;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_EXTRACT_IMAGE = AVS_OFFICESTUDIO_FILE_OTHER + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_MS_OFFCRYPTO = AVS_OFFICESTUDIO_FILE_OTHER + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_HTMLZIP = AVS_OFFICESTUDIO_FILE_OTHER + 0x0003;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_OLD_DOCUMENT = AVS_OFFICESTUDIO_FILE_OTHER + 0x0004;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_OLD_PRESENTATION = AVS_OFFICESTUDIO_FILE_OTHER + 0x0005;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_OLD_DRAWING = AVS_OFFICESTUDIO_FILE_OTHER + 0x0006;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_TEAMLAB_INNER = AVS_OFFICESTUDIO_FILE_OTHER + 0x0007;
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_JSON = AVS_OFFICESTUDIO_FILE_OTHER + 0x0008; // Для mail-merge
+	public final static int AVS_OFFICESTUDIO_FILE_OTHER_PDFA = AVS_OFFICESTUDIO_FILE_OTHER + 0x0101;
+	public final static int AVS_OFFICESTUDIO_FILE_TEAMLAB = 0x1000;
+	public final static int AVS_OFFICESTUDIO_FILE_TEAMLAB_DOCY = AVS_OFFICESTUDIO_FILE_TEAMLAB + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_TEAMLAB_XLSY = AVS_OFFICESTUDIO_FILE_TEAMLAB + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_TEAMLAB_PPTY = AVS_OFFICESTUDIO_FILE_TEAMLAB + 0x0003;
+	public final static int AVS_OFFICESTUDIO_FILE_CANVAS = 0x2000;
+	public final static int AVS_OFFICESTUDIO_FILE_CANVAS_WORD = AVS_OFFICESTUDIO_FILE_CANVAS + 0x0001;
+	public final static int AVS_OFFICESTUDIO_FILE_CANVAS_SPREADSHEET = AVS_OFFICESTUDIO_FILE_CANVAS + 0x0002;
+	public final static int AVS_OFFICESTUDIO_FILE_CANVAS_PRESENTATION = AVS_OFFICESTUDIO_FILE_CANVAS + 0x0003;
+
+	public final static int NO_ERROR = 0;
+	public final static int UNKNOWN = -1;
+	public final static int READ_REQUEST_STREAM = -3;
+	public final static int WEB_REQUEST = -4;
+	public final static int TASK_QUEUE = -20;
+	public final static int TASK_RESULT = -40;
+	public final static int STORAGE = -60;
+	public final static int STORAGE_FILE_NO_FOUND = -61;
+	public final static int STORAGE_READ = -62;
+	public final static int STORAGE_WRITE = -63;
+	public final static int STORAGE_REMOVE_DIR = -64;
+	public final static int STORAGE_CREATE_DIR = -65;
+	public final static int STORAGE_GET_INFO = -66;
+	public final static int CONVERT = -80;
+	public final static int CONVERT_DOWNLOAD = -81;
+	public final static int CONVERT_UNKNOWN_FORMAT = -82;
+	public final static int CONVERT_TIMEOUT = -83;
+	public final static int CONVERT_READ_FILE = -84;
+	public final static int CONVERT_CORRUPTED = -86;
+	public final static int CONVERT_LIBREOFFICE = -87;
+	public final static int CONVERT_PARAMS = -88;
+	public final static int CONVERT_NEED_PARAMS = -89;
+	public final static int CONVERT_DRM = -90;
+	public final static int CONVERT_PASSWORD = -91;
+	public final static int CONVERT_ICU = -92;
+	public final static int CONVERT_LIMITS = -93;
+	public final static int CONVERT_DEAD_LETTER = -99;
+	public final static int UPLOAD = -100;
+	public final static int UPLOAD_CONTENT_LENGTH = -101;
+	public final static int UPLOAD_EXTENSION = -102;
+	public final static int UPLOAD_COUNT_FILES = -103;
+	public final static int UPLOAD_URL = -104;
+	public final static int VKEY = -120;
+	public final static int VKEY_ENCRYPT = -121;
+	public final static int VKEY_KEY_EXPIRE = -122;
+	public final static int VKEY_USER_COUNT_EXCEED = -123;
+	public final static int VKEY_TIME_EXPIRE = -124;
+	public final static int VKEY_TIME_INCORRECT = -125;
+	public final static int EDITOR_CHANGES = -160;
+	
+	public class RIGHTS {
+		public final static int None  = 0;
+		public final static int Edit  = 1;
+		public final static int Review = 2;
+		public final static int Comment = 3;
+		public final static int View = 4;
+	}
+	
+	public class LICENSE_RESULT  {
+		public final static int Error  = 1;
+		public final static int Expired = 2;
+		public final static int Success = 3;
+		public final static int UnknownUser = 4;
+		public final static int Connections  = 5;
+		public final static int ExpiredTrial = 6;
+		public final static int SuccessLimit = 7;
+		public final static int UsersCount   = 8;
+		public final static int ConnectionsOS = 9;
+		public final static int UsersCountOS = 10;
+	}
+	
+	public class LICENSE_MODE  {
+		public final static int None  = 0;
+		public final static int Trial = 1;
+		public final static int Developer = 2;
+	}
+	
+	public final static int LICENSE_CONNECTIONS = 20;
+	public final static int LICENSE_EXPIRE_USERS_ONE_DAY = 24 * 60 * 60; // day in seconds
+	
+	public final static int PACKAGE_TYPE_OS = 0;
+	public final static int PACKAGE_TYPE_I = 1;
+	public final static int PACKAGE_TYPE_D = 2;	
+}
