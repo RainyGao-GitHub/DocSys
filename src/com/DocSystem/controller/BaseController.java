2dbbbde4b1a9db496b670367dfe31f60c4786821
yuan <13777479349@139.com>
2021-03-18 17:24:54 +0800

    优化升级代码，但是仍然无法工作

--- src/com/DocSystem/controller/BaseController.java
+++ src/com/DocSystem/controller/BaseController.java
@@ -1,6 +1,7 @@
 package com.DocSystem.controller;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -9861,6 +9863,33 @@ public class BaseController  extends BaseFunction{
         return run(restartCmd, null, null) != null;
     }
     
+	/**************************** Tomcat控制接口 
+	 * @param rt *********************************/
+    public static boolean upgradeServer(ReturnAjax rt) {              
+    	String serverPath = Path.getParentPath(docSysWebPath, 3, OSType);
+    	if(serverPath == null)
+		{
+			System.out.println("restartTomcat() Failed to get serverPath");
+			rt.setError("获取服务器路径失败！");
+			return false;
+		}
+    	
+    	String restartScriptPath = serverPath + "upgrade.sh";
+    	if(isWinOS())
+    	{
+    		restartScriptPath = serverPath + "upgrade.bat";
+    	}
+    	
+    	File file = new File(restartScriptPath);
+    	if(file.exists() == false){
+    		rt.setError("找不到服务器升级脚本！");
+    		return false;
+    	}
+    	
+    	String restartCmd = buildScriptRunCmd(restartScriptPath);        
+        return run(restartCmd, null, null) != null;
+    }
+    
 	public static boolean restartTomcat(String tomcatPath, String javaHome) {              
     	String stopScriptPath = generateTomcatStopScript(tomcatPath, javaHome);
     	if(stopScriptPath == null)
@@ -10234,6 +10263,8 @@ public class BaseController  extends BaseFunction{
 		}
 		return false;
 	}
+	
+
 
 	private Doc checkAndGetRealParentCompressDoc(Repos repos, Doc rootDoc, Doc parentCompressDoc) {
 		File parentFile = new File(parentCompressDoc.getLocalRootPath() + parentCompressDoc.getPath() + parentCompressDoc.getName());
@@ -10967,6 +10998,83 @@ public class BaseController  extends BaseFunction{
 		}		
 		return ret;
 	}
+	
+	 public static boolean unZip(String path, String savepath) 
+	 { 
+		boolean ret = false;
+	    int count = -1; 
+	    InputStream is = null; 
+	    FileOutputStream fos = null; 
+	    BufferedOutputStream bos = null; 
+	    
+	    File file = new File(savepath);
+	    if(file.exists() == false)
+	    {
+	    	file.mkdir(); //创建保存目录 
+	    }
+	    ZipFile zipFile = null; 
+	    try
+	    { 
+	      zipFile = new ZipFile(path,"gbk"); //解决中文乱码问题 
+	      Enumeration<?> entries = zipFile.getEntries(); 
+	      while(entries.hasMoreElements()) 
+	      { 
+			byte buf[] = new byte[2048]; 
+	        ZipEntry entry = (ZipEntry)entries.nextElement(); 
+	        String filename = entry.getName(); 
+	        boolean ismkdir = false; 
+	        if(filename.lastIndexOf("/") != -1){ //检查此文件是否带有文件夹 
+	         ismkdir = true; 
+	        } 
+	        filename = savepath + filename; 
+	        if(entry.isDirectory()){ //如果是文件夹先创建 
+	         file = new File(filename); 
+	         file.mkdirs(); 
+	          continue; 
+	        } 
+	        file = new File(filename); 
+	        if(!file.exists()){ //如果是目录先创建 
+	         if(ismkdir){ 
+	         new File(filename.substring(0, filename.lastIndexOf("/"))).mkdirs(); //目录先创建 
+	         } 
+	        } 
+	        file.createNewFile(); //创建文件 
+	        is = zipFile.getInputStream(entry); 
+	        fos = new FileOutputStream(file); 
+	        bos = new BufferedOutputStream(fos, 2048); 
+	        while((count = is.read(buf)) > -1) 
+	        { 
+	          bos.write(buf, 0, count); 
+	        } 
+	        bos.flush(); 
+	        bos.close(); 
+	        fos.close(); 
+	        is.close(); 
+	      } 
+	      zipFile.close(); 
+	      ret = true;
+	    }catch(IOException ioe){ 
+	      ioe.printStackTrace(); 
+	    }finally{ 
+	       try{ 
+	       if(bos != null){ 
+	         bos.close(); 
+	       } 
+	       if(fos != null) { 
+	         fos.close(); 
+	       } 
+	       if(is != null){ 
+	         is.close(); 
+	       } 
+	       if(zipFile != null){ 
+	         zipFile.close(); 
+	       } 
+	       }catch(Exception e) { 
+	         e.printStackTrace(); 
+	       } 
+	    } 
+	    return ret;
+	} 
 
 	//获取doc的parentZipDoc(这里有风险parentZipDoc里面的目录的名字带压缩后缀)
 	private Doc getParentCompressDoc(Repos repos, Doc rootDoc, Doc doc) {
