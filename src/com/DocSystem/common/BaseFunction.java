c5691903b660d957c8316d3e3ea97eca30e1f147
yuan <13777479349@139.com>
2021-04-01 11:56:08 +0800

    add log

--- src/com/DocSystem/common/BaseFunction.java
+++ src/com/DocSystem/common/BaseFunction.java
@@ -644,6 +644,7 @@ public class BaseFunction{
     {
 		SystemLog log = new SystemLog();
 		log.time = new Date().getTime();
+		log.ip = getRequestIpAddress(request);
 		log.userId = user.getId() + "";
 		log.userName = user.getName();
 		log.event = event;
@@ -786,6 +787,25 @@ public class BaseFunction{
 		}
     }  
 	
+	protected static ArrayList<SystemLog> getSystemLogList(SystemLog qLog, Long startTime, Long endTime)
+	{
+		Date startDate = new Date(startTime);
+		Date endDate = new Date(endTime);
+		ArrayList<SystemLog> result = new ArrayList<SystemLog>();
+		for(int month = startDate.getMonth(); month <= endDate.getMonth(); month ++)
+		{
+			String indexLibName = "SystemLog-" + startDate.getYear() + "-" + month;
+			String indexLib = getSystemLogStorePath() + indexLibName + "/";
+			List<SystemLog> list = getSystemLogList(qLog, indexLib);
+			if(list != null)
+			{
+				result.addAll(list);
+			}
+		}
+		return result;
+				
+	}
+	
 	protected static ArrayList<SystemLog> getSystemLogList(SystemLog qLog, String indexLib)
 	{
 		ArrayList<SystemLog> list = new ArrayList<SystemLog>();
@@ -880,5 +900,48 @@ public class BaseFunction{
         }
         return path;
     }	
+	
+	protected static String getRequestIpAddress(HttpServletRequest request) {
+	    String ip = null;
+
+	    //X-Forwarded-For：Squid 服务代理
+	    String ipAddresses = request.getHeader("X-Forwarded-For");
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) 
+	    {
+	        //Proxy-Client-IP：apache 服务代理
+	        ipAddresses = request.getHeader("Proxy-Client-IP");
+	    }
+	    
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) 
+	    {
+	        //WL-Proxy-Client-IP：weblogic 服务代理
+	        ipAddresses = request.getHeader("WL-Proxy-Client-IP");
+	    }
+	    
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) 
+	    {
+	        //HTTP_CLIENT_IP：有些代理服务器
+	        ipAddresses = request.getHeader("HTTP_CLIENT_IP");
+	    }
+	    
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) 
+	    {
+	        //X-Real-IP：nginx服务代理
+	        ipAddresses = request.getHeader("X-Real-IP");
+	    }
+
+	    //有些网络通过多层代理，那么获取到的ip就会有多个，一般都是通过逗号（,）分割开来，并且第一个ip为客户端的真实IP
+	    if (ipAddresses != null && ipAddresses.length() != 0) {
+	        ip = ipAddresses.split(",")[0];
+	    }
+
+	    //还是不能获取到，最后再通过request.getRemoteAddr();获取
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) 
+	    {
+	        ip = request.getRemoteAddr();
+	    }
+	    
+	    return ip.equals("0:0:0:0:0:0:0:1")?"127.0.0.1":ip;
+	}
 	
 }
