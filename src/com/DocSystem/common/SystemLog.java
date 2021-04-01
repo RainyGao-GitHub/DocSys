adf6ed3747a8bf1fde29853447f124fb3cf4b0ed
yuan <13777479349@139.com>
2021-04-01 09:53:10 +0800

    add log

---
+++ src/com/DocSystem/common/SystemLog.java
@@ -0,0 +1,14 @@
+package com.DocSystem.common;
+
+public class SystemLog {
+
+	public String id;
+	public Long time;
+	public String ip;
+	public String userId;
+	public String userName;
+	public String event;
+	public String subEvent;
+	public String content;
+
+}
