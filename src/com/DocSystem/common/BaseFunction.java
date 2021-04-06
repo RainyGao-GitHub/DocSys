e2cf45fa89d1849dcfbd29d93b3470091cb7e358
yuan <13777479349@139.com>
2021-04-06 15:41:18 +0800

    license check

--- src/com/DocSystem/common/BaseFunction.java
+++ src/com/DocSystem/common/BaseFunction.java
@@ -28,6 +28,8 @@ import org.apache.lucene.util.Version;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import util.ReadProperties;
+import util.ReturnAjax;
+
 import com.DocSystem.common.CommonAction.CommonAction;
 import com.DocSystem.common.constants.LICENSE_RESULT;
 import com.DocSystem.commonService.ProxyThread;
@@ -80,6 +82,28 @@ public class BaseFunction{
 		systemLicenseInfo.usersCount = null;	//无限制
 		systemLicenseInfo.expireTime = null; //长期有效
 		systemLicenseInfo.hasLicense = false;
+	}
+	
+	protected boolean systemLicenseInfoCheck(ReturnAjax rt) {
+		if(systemLicenseInfo.expireTime != null)
+		{	
+			long curTime = new Date().getTime();
+			if(systemLicenseInfo.expireTime < curTime) 
+			{
+				rt.setError("证书已过期，请购买商业版证书！");
+				return false;	
+			}
+		}
+		if(systemLicenseInfo.state != null)
+		{	
+			if(systemLicenseInfo.state == 0) 
+			{
+				rt.setError("证书已失效，请重新购买商业版证书！");
+				return false;	
+			}
+		}
+		
+		return true;
 	}
 
 	private static void initOfficeLicenseInfo() {
