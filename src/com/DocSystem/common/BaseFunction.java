4ca76d768602773deb1e67d31dd13a1785fbd506
yuan <13777479349@139.com>
2021-05-10 09:29:06 +0800

    Add definitio for second and minute

--- src/com/DocSystem/common/BaseFunction.java
+++ src/com/DocSystem/common/BaseFunction.java
@@ -43,6 +43,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 public class BaseFunction{	
+	protected static final long CONST_SECOND = 1000;
+	protected static final long CONST_MINUTE = 60*1000;
 	protected static final long CONST_HOUR = 60*60*1000;
 	protected static final long CONST_DAY = 24*CONST_HOUR;
 	protected static final long CONST_MONTH = 30*CONST_DAY;
