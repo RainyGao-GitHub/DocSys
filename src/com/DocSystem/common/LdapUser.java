844f307a598f79754973b768616c2c3b7312cab8
yuan <13777479349@139.com>
2021-06-25 17:36:55 +0800

    add LdapUser.java

---
+++ src/com/DocSystem/common/LdapUser.java
@@ -0,0 +1,15 @@
+package com.DocSystem.common;
+
+
+public class LdapUser {
+ 
+	public String cn;
+	public String sn;
+	public String uid;
+	public String userPassword;
+	public String displayName;
+	public String mail;
+	public String description;
+	public String uidNumber;
+	public String gidNumber;
+}
