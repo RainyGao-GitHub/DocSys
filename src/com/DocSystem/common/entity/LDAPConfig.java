1b63b956e15ae4acc6ab9d4362af974bdc0963ae
yuan <13777479349@139.com>
2021-07-03 23:07:17 +0800

    LDAP登录优化：支持authMode指定

--- src/com/DocSystem/common/entity/LDAPConfig.java
+++ src/com/DocSystem/common/entity/LDAPConfig.java
@@ -1,7 +1,11 @@
 package com.DocSystem.common.entity;
 
+import com.alibaba.fastjson.JSONObject;
+
 public class LDAPConfig {
 	public Boolean enabled;
 	public String url;
 	public String basedn;
+	public JSONObject settings;
+	public Integer authMode;
 }
