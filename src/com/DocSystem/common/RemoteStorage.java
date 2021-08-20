d146c0dd43e583e3e8d1c162de08c7c4c7c69c68
yuan <13777479349@139.com>
2021-08-20 17:30:24 +0800

    自动推送和拉取代码优化

--- src/com/DocSystem/common/RemoteStorage.java
+++ src/com/DocSystem/common/RemoteStorage.java
@@ -4,6 +4,9 @@ public class RemoteStorage {
 
 	public String protocol;
 	public Integer autoPull;
+	public boolean autoPullForce = false;
+	public Integer autoPush;
+	public boolean autoPushForce = false;
 	public SftpConfig SFTP;
 	public FtpConfig FTP;
 	public String rootPath;
