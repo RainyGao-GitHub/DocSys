99c2580336c826f3a8de3a9918ccdf35a744008c
yuan <13777479349@139.com>
2021-08-24 15:52:04 +0800

    远程存储优化

--- src/com/DocSystem/common/entity/DocPushResult.java
+++ src/com/DocSystem/common/entity/DocPushResult.java
@@ -1,7 +1,14 @@
 package com.DocSystem.common.entity;
 
+import java.util.List;
+
+import com.DocSystem.common.CommitAction.CommitAction;
+
 public class DocPushResult {
 	public Integer successCount;
 	public Integer failCount;
 	public Integer totalCount;
+	public List<CommitAction> actionList;	
+	public boolean isSubAction = false;
+	public CommitAction action; //latest add commitAction
 }
