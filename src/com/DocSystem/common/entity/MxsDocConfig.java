5b836c17a722b2d00edb6c6d85c8ad8261ad1044
yuan <13777479349@139.com>
2021-09-16 10:37:51 +0800

    MxsDoc远程存储优化

--- src/com/DocSystem/common/entity/MxsDocConfig.java
+++ src/com/DocSystem/common/entity/MxsDocConfig.java
@@ -4,5 +4,6 @@ public class MxsDocConfig {
 	public String userName;
 	public String pwd;
 	public String url;
-	public Integer type = 1;	//1: 仓库 2:服务器目录 
+	public Integer reposId;
+	public String remoteDirectory;
 }
