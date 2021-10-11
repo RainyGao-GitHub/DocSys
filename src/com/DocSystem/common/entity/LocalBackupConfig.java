5529c915e44914bb4f6ca8fcb681544e2430341a
yuan <13777479349@139.com>
2021-10-11 16:44:53 +0800

    自动备份
    远程存储配置支持本地存储

--- src/com/DocSystem/common/entity/LocalBackupConfig.java
+++ src/com/DocSystem/common/entity/LocalBackupConfig.java
@@ -3,5 +3,6 @@ package com.DocSystem.common.entity;
 public class LocalBackupConfig {
 	public String rootPath;	//localRootPath
 	public Integer realTimeBackupEn = 0;
-	public Integer fullBackupEn = 0;	
+	public Integer fullBackupEn = 0;
+	public RemoteStorageConfig remoteStorageConfig;	
 }
