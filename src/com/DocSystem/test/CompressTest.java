da80eb90292644bbfb3b1104df738cb37c62d5b0
yuan <13777479349@139.com>
2021-10-21 14:08:14 +0800

    [BugFix] [目录压缩] 目录压缩后中文乱码
    使用7z进行压缩

--- src/com/DocSystem/test/CompressTest.java
+++ src/com/DocSystem/test/CompressTest.java
@@ -5,8 +5,8 @@ import com.DocSystem.common.FileUtil;
 
 public class CompressTest {
 	public static void main(String[] args) throws IOException {
-		String path = "C:/Test/TestDir";
-		String outPath = "C:/Test/Test.zip";
+		String path = "C:/Test/临时目录";
+		String outPath = "C:/Test/临时目录.zip";
 		FileUtil.compressWith7z(path, outPath);
 	}	
 }
