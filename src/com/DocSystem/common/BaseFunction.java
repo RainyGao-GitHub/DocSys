7433f5ab895a24a4c97bc9ed724f1097c50bee3e
yuan <13777479349@139.com>
2021-11-26 10:28:14 +0800

    [BugFix] 自动备份线程调用DocUtil类出现异常
    1. copy buildBasicDoc to BaseFunction

--- src/com/DocSystem/common/BaseFunction.java
+++ src/com/DocSystem/common/BaseFunction.java
@@ -14,7 +14,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -1160,16 +1159,105 @@ public class BaseFunction{
 		return true;
 	}
 	
+	//注意：该接口和DocUtil中的buildBasicDoc是一样的，在这里定义是因为自动备份线程调用DocUtil类时
+	public static Doc buildBasicDocBase(Integer reposId, Long docId, Long pid, String reposPath, String path, String name, 
+			Integer level, Integer type, boolean isRealDoc, String localRootPath, String localVRootPath, Long size, String checkSum,
+			String offsetPath) 
+	{
+		//Format path and name
+		if(reposPath == null)
+		{
+			reposPath = "";
+		}
+		
+		if(path == null)
+		{
+			path = "";
+		}
+		if(name == null)
+		{
+			name = "";
+		}
+		if(offsetPath == null)
+		{
+			offsetPath = "";
+		}
+		
+		//To support user call the interface by entryPath
+		if(name.isEmpty())
+		{
+			if(!path.isEmpty())
+			{
+				String[] temp = new String[2]; 
+				level = Path.seperatePathAndName(path, temp);
+				path = temp[0];
+				name = temp[1];			
+			}
+		}
+		
+		//在仓库管理界面，为了能够返回然根节点信息带有仓库名字，导致传入的Name不为空，这是一个错误的决定
+		if(name.isEmpty())	//rootDoc
+		{
+			level = -1;
+			docId = 0L;
+			pid = -1L;
+		}
+		
+		if(level == null)
+		{
+			level = Path.getLevelByParentPath(path);
+		}
+		
+		Doc doc = new Doc();
+		doc.setVid(reposId);
+		doc.setReposPath(reposPath);
+		doc.setPath(path);
+		doc.setName(name);
+		doc.setLevel(level);
+		doc.setType(type);
+		doc.setLocalRootPath(localRootPath);
+		doc.setLocalVRootPath(localVRootPath);
+		doc.setSize(size);
+		doc.setCheckSum(checkSum);
+		
+		doc.setIsRealDoc(isRealDoc);
+		doc.offsetPath = offsetPath;
+		
+		if(isRealDoc)
+		{
+			if(docId == null)
+			{
+				docId = Path.buildDocIdByName(level, path, name);
+			}
+			
+			if(pid == null)
+			{
+				if(path.isEmpty())
+				{
+					pid = 0L;
+				}
+				else
+				{
+					pid = Path.buildDocIdByName(level-1, path, "");
+				}
+			}
+		}
+
+		doc.setDocId(docId);
+		doc.setPid(pid);
+		return doc;
+	}
+	
 	public static Doc buildBasicDoc(Integer reposId, Long docId, Long pid, String reposPath, String path, String name, Integer level, Integer type, boolean isRealDoc, String localRootPath, String localVRootPath, Long size, String checkSum) 
 	{
-		Doc doc = DocUtil.buildBasicDoc(reposId, docId, pid, reposPath, path, name, level, type, isRealDoc, localRootPath, localVRootPath, size, checkSum, "");
+		Doc doc = buildBasicDocBase(reposId, docId, pid, reposPath, path, name, level, type, isRealDoc, localRootPath, localVRootPath, size, checkSum, "");
 		doc.isBussiness = systemLicenseInfo.hasLicense;
 		return doc;
 	}
 	
 	public static Doc buildBasicDoc(Integer reposId, Long docId, Long pid, String reposPath, String path, String name, Integer level, Integer type, boolean isRealDoc, String localRootPath, String localVRootPath, Long size, String checkSum, String offsetPath) 
 	{
-		Doc doc = DocUtil.buildBasicDoc(reposId, docId, pid, reposPath, path, name, level, type, isRealDoc, localRootPath, localVRootPath, size, checkSum, offsetPath);
+		Doc doc = buildBasicDocBase(reposId, docId, pid, reposPath, path, name, level, type, isRealDoc, localRootPath, localVRootPath, size, checkSum, offsetPath);
 		doc.isBussiness = systemLicenseInfo.hasLicense;
 		return doc;
 	}
