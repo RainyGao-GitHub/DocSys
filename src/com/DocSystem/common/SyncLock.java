07d91949be836c9a078f189440742418940d90bd
yuan <13777479349@139.com>
2022-05-13 14:00:10 +0800

    同步锁使用优化
    1. 去掉唤醒调用，因为目前不需要wait的逻辑

--- src/com/DocSystem/common/SyncLock.java
+++ src/com/DocSystem/common/SyncLock.java
@@ -1,19 +1,22 @@
 package com.DocSystem.common;
 
 public class SyncLock {
+	public static void lock(String lockInfo) {
+		// TODO Auto-generated method stub
+		Log.debug("\n********** " + lockInfo + " lock ++++++");
+	}  
+	
 	public static void unlock(Object syncLock, String lockInfo) {
-		syncLock.notifyAll();//唤醒等待线程
-		Log.debug(lockInfo + " unlock -------");																
-		//下面这段代码是因为参考了网上的一个Demo说wait是释放锁，我勒了个区去，留着作纪念
+		Log.debug("********** " + lockInfo + " unlock -------\n");																
+		
+		//syncLock.notifyAll();//唤醒等待线程（如果有其他线程调用过wait，才需要唤醒）
+		
+		//wait是表示当前线程需要等待某些资源主动放弃锁，需要其他线程调用notify或notifyAll来唤醒
+		//下面这段代码是因为参考了网上的一个Demo说wait是释放锁，所以实际上是个错误的做法，留着作纪念
 		//try {
-		//	syncLock.wait();	//线程睡眠，等待syncLock.notify/notifyAll唤醒
+		//	syncLock.wait();	//线程睡眠，等待其他线程调用syncLock.notify/notifyAll唤醒
 		//} catch (InterruptedException e) {
 		//	e.printStackTrace();
 		//}
 	}
-
-	public static void lock(String lockInfo) {
-		// TODO Auto-generated method stub
-		Log.debug(lockInfo + " lock ++++++");
-	}  
 }
