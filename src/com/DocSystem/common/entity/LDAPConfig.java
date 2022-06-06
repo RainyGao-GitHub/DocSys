31e74d0e891ca69a1f72555fa57524d64e2f65ae
yuan <13777479349@139.com>
2022-06-06 16:04:52 +0800

    [Enhancement] LDAP配置优化
    1. 增加鉴权参数authentication

--- src/com/DocSystem/common/entity/LDAPConfig.java
+++ src/com/DocSystem/common/entity/LDAPConfig.java
@@ -7,9 +7,10 @@ public class LDAPConfig {
 	public String url;
 	public String basedn;
 	public JSONObject settings;
-	public Integer authMode;
+	public String authentication; //simple, GSSAPI
+	public Integer authMode;	//密码模式
 	public String loginMode;	//用户名字段属性
-	public String userAccount;	//直接指定LDAPConnect使用的account
-	public String userPassword;
-	public String filter;
+	public String userAccount;	//LDAP管理员账号（如果指定了，则使用该用户鉴权）
+	public String userPassword;	//LDAP管理员的密码
+	public String filter;  		//过滤条件
 }
