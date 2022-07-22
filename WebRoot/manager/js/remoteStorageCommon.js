df84d7327052754f195d1a12da931720360b8f77
yuan <13777479349@139.com>
2022-07-22 22:29:49 +0800

    FTP服务器提示增加isPassive参数

--- WebRoot/manager/js/remoteStorageCommon.js
+++ WebRoot/manager/js/remoteStorageCommon.js
@@ -6,7 +6,7 @@ function getPlacehoderForRemoteStorageProtocol(protocol)
 	switch(protocol)
 	{
 	case "ftp":
-		str = 'ftp://192.168.0.1:21/path;userName=test;pwd=123456;charset=utf-8;';
+		str = 'ftp://192.168.0.1:21/path;userName=test;pwd=123456;;charset=UTF-8;isPassive=true;';
 		break;
 	case "sftp":
 		str = 'sftp://192.168.0.1:22/path;userName=test;pwd=123456;';
