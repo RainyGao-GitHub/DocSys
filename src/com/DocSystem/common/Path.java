70cc9b5e554af4e7ce28d83adb76153130be9be5
yuan <13777479349@139.com>
2022-07-28 20:47:13 +0800

    BugFix: getLevelByParentPath level计算错误，导致dbUpdateDoc出现非法docId

--- src/com/DocSystem/common/Path.java
+++ src/com/DocSystem/common/Path.java
@@ -29,7 +29,7 @@ public class Path {
 		
 		String [] paths = path.split("/");
 		int level = 0;
-		for(int i=0; i>=paths.length; i++)
+		for(int i=0; i < paths.length; i++)
 		{
 			if(paths[i].isEmpty())
 			{
