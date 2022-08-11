d8cdfa86180491d84d1c2e59defcbf904addd315
yuan <13777479349@139.com>
2022-08-11 13:58:16 +0800

    [CleanCode] 新增仓库和修改仓库设置代码优化
    1. copy js from web to manager

---
+++ WebRoot/manager/js/MyJquery.js
@@ -0,0 +1,95 @@
+//MyJquery类
+var MyJquery = (function () {
+  	function getValue(id)
+	{
+  		return $("#" + id).val();
+	}
+  	
+  	function setValue(id, value)
+	{
+  		$("#" + id).val(value);
+	}
+  	
+  	function getText(id)
+	{
+  		return $("#" + id).text();
+	}
+  	
+  	function setText(id, text)
+	{
+  		$("#" + id).text(text);
+	}
+  	
+  	function disable(id)
+	{
+  		$("#" + id).attr("disabled","disabled");
+	}
+
+  	function enable(id)
+	{
+  		$("#" + id).attr("disabled",false);
+	}
+  	
+  	function isChecked(id)
+  	{
+  		return $("#" + id).is(':checked')? 1: 0;
+  	}	
+  	
+  	function hide(id)
+  	{
+  		return $("#" + id).hide();
+  	}	
+
+  	function show(id)
+  	{
+  		return $("#" + id).show();
+  	}	
+  	
+  	function focus(id)
+  	{
+  		return $("#" + id).focus();
+  	}	
+  	
+  	function select(id, index)
+  	{
+  		return $("#" + id).get(0).selectedIndex=index;
+  	}
+
+	//开放给外部的调用接口
+    return {
+    	getValue: function(id){
+    		return getValue(id);
+        },    
+    	setValue: function(id,value){
+    		setValue(id,value);
+        },
+    	getText: function(id){
+    		return getText(id);
+        },    
+    	setText: function(id,text){
+    		setText(id,text);
+        },
+        disable: function(id){
+        	disable(id);
+        },   
+        enable: function(id){
+        	enable(id);
+        },   
+        isChecked: function(id){
+        	return isChecked(id);
+        },
+        hide: function(id){
+        	hide(id);
+        }, 
+        show: function(id){
+        	show(id);
+        },        
+        focus: function(id){
+        	focus(id);
+        }, 
+        select: function(id, index){
+        	select(id, index);
+        }, 
+    };
+})();
+    
