f6b3dccfd12141bc240fe155b10cd53187cbb55f
yuan <13777479349@139.com>
2022-08-24 17:34:07 +0800

    增加服务器mac地址获取代码

--- src/com/DocSystem/common/IPUtil.java
+++ src/com/DocSystem/common/IPUtil.java
@@ -2,6 +2,8 @@ package com.DocSystem.common;
 
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,6 +21,42 @@ public class IPUtil {
 		return IP;
 	}
 	
+	public static String getMacAddress() {
+		String mac = null;
+		try {
+			InetAddress ip4 = Inet4Address.getLocalHost();
+			mac = getLocalMac(ip4);
+			Log.debug("getMacAddress() mac:" + mac);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+		return mac;
+	}
+	
+	private static String getLocalMac(InetAddress ip) {
+		String macStr = "00-00-00-00-00-00";
+		try {
+			NetworkInterface networkInterface = NetworkInterface.getByInetAddress(ip);
+			byte[] mac = networkInterface.getHardwareAddress();
+			if(mac != null && mac.length > 0)
+			{
+				macStr = "";
+				for(int i=0; i<mac.length; i++)
+				{
+					if(i!=0)
+					{
+						macStr += "-";
+					}
+					int temp = mac[i] & 0xFF;
+					macStr += Integer.toHexString(temp);
+				}
+			}			
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		return macStr;
+	}
+
 	public static String getIpAddress(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
