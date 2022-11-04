97c1ed8cdac8e1b8c8e5f9e00b0a3974ada1c0ba
yuan <13777479349@139.com>
2022-11-04 16:20:27 +0800

    【集群】redis 的数据必须可以序列化为二进制
    1.add implements Serializable for object which need to stored in redis

--- src/com/DocSystem/common/TextSearchConfig.java
+++ src/com/DocSystem/common/TextSearchConfig.java
@@ -1,8 +1,13 @@
 package com.DocSystem.common;
 
+import java.io.Serializable;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class TextSearchConfig {
+public class TextSearchConfig  implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1899770164996730777L;
 	public Boolean enable = false;
 	public ConcurrentHashMap<String, Integer> realDocTextSearchDisableHashMap = null;	
 	public ConcurrentHashMap<String, Integer> virtualDocTextSearchDisablehHashMap = null;	
--- src/com/DocSystem/common/entity/SvnConfig.java
+++ src/com/DocSystem/common/entity/SvnConfig.java
@@ -1,7 +1,13 @@
 package com.DocSystem.common.entity;
 
-public class SvnConfig {
+import java.io.Serializable;
 
+public class SvnConfig  implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5372669359020710841L;
 	public Integer isRemote;
 	public String userName;
 	public String pwd;
