cb63eee1ba78c31bdf5513d0a12df72454300a7d
yuan <13777479349@139.com>
2022-11-05 20:22:51 +0800

    【集群】ReposAccess增加序列化支持

--- src/com/DocSystem/common/entity/ReposAccess.java
+++ src/com/DocSystem/common/entity/ReposAccess.java
@@ -1,10 +1,16 @@
 package com.DocSystem.common.entity;
 
+import java.io.Serializable;
+
 import com.DocSystem.entity.DocAuth;
 import com.DocSystem.entity.DocShare;
 import com.DocSystem.entity.User;
 
-public class ReposAccess {
+public class ReposAccess   implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8420401345353830591L;
 	private Integer accessUserId = null; //
 	private User accessUser = null; //
 	private DocAuth authMask = null;	//
