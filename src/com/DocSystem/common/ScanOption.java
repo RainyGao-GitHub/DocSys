37c242d14ef0208fedb1445f63d57e2dd9de669d
yuan <13777479349@139.com>
2022-11-14 16:18:55 +0800

    【优化】仓库同步扫描优化
    1. isRemoteChanged return is incorrect

--- src/com/DocSystem/common/ScanOption.java
+++ src/com/DocSystem/common/ScanOption.java
@@ -3,6 +3,8 @@ package com.DocSystem.common;
 public class ScanOption {
 
 	public int scanType;
+
+	public long scanTime;
 	
 	public String localChangesRootPath = null;	//本地改动的文件节点存储根路径
 	public String remoteChangesRootPath = null; //改动的文件节点存储根路径
