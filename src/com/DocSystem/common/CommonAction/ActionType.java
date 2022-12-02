3ff06a2909ef9170a9cb98519e69120249137e84
yuan <13777479349@139.com>
2022-12-02 23:37:13 +0800

    【优化】目录删除、移动、复制性能优化
    1. 增加VFS接口 for copy and move
    2. add IndexForAll for delete

--- src/com/DocSystem/common/CommonAction/ActionType.java
+++ src/com/DocSystem/common/CommonAction/ActionType.java
@@ -2,9 +2,10 @@ package com.DocSystem.common.CommonAction;
 
 public enum ActionType {
 	UNDEFINED,
-	FS,
+	FS,			
 	VERREPOS,
 	DB,
 	INDEX,
-	AUTOSYNCUP;
+	AUTOSYNCUP,
+	VFS;		//Doc的VirtualDoc操作（如果Doc是目录，则会操作其子目录的VirtualDoc，操作完后会提交至VerRepos）
 }
