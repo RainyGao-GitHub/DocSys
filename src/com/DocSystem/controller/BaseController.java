53f7fd90001747846666016b533721de935189fd
yuan <13777479349@139.com>
2022-12-05 15:06:21 +0800

    【BugFix】文件删除、移动、复制、重命名增加访问密码检查

--- src/com/DocSystem/controller/BaseController.java
+++ src/com/DocSystem/controller/BaseController.java
@@ -22080,4 +22080,21 @@ public class BaseController  extends BaseFunction{
 		Log.debug("buildSystemLogDetailContent() logDetail:" + logDetail);
 		return logDetail;
 	}
+	
+	protected boolean checkUserAccessPwd(Repos repos, Doc doc, HttpSession session, ReturnAjax rt) {
+		String pwd = getDocPwd(repos, doc);
+		if(pwd != null && !pwd.isEmpty())
+		{
+			//Do check the sharePwd
+			String docPwd = (String) session.getAttribute("docPwd_" + repos.getId() + "_" + doc.getDocId());
+			if(docPwd == null || docPwd.isEmpty() || !docPwd.equals(pwd))
+			{
+				docSysErrorLog("访问密码错误！", rt);
+				rt.setMsgData("1"); //访问密码错误或未提供
+				rt.setData(doc);
+				return false;
+			}
+		}
+		return true;
+	}
 }
