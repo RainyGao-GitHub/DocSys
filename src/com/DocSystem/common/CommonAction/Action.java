944dd555f825ac57d030fad2c8c1b1728eb36833
yuan <13777479349@139.com>
2022-12-13 12:30:41 +0800

    syncUpForDocChange 需要指定actionType

--- src/com/DocSystem/common/CommonAction/Action.java
+++ src/com/DocSystem/common/CommonAction/Action.java
@@ -8,8 +8,11 @@ public enum Action {
 	MOVE,
 	COPY,
 	PUSH,
-	SYNC_ALL, 				//同步远程存储、版本仓库、刷新索引（包括子目录）
-	SYNC_ALL_FORCE, 		//同步远程存储、版本仓库、刷新索引（包括子目录）
-	SYNC_VerRepos, 			//只同步版本仓库
-	SYNC_VerReposAndIndex; 	//只同步版本仓库和索引（远程拉取时调用）	
+	SYNC_ALL, 					//同步远程存储、版本仓库、强行刷新索引   （包括子目录）
+	SYNC_ALL_FORCE, 			//同步远程存储、版本仓库、强行刷新索引   （包括子目录）
+	SYNC_AUTO, 					//同步远程存储、版本仓库、不更新索引（包括子目录），不更新索引是为了保证性能
+	SYNC_AfterRevertHistory,	//同步版本仓库、更新有改动文件的索引（禁用远程拉取，避免恢复的文件被覆盖）
+	SYNC_AfterRemoteStoragePull,//同步版本仓库、更新有改动文件的索引（禁用远程拉取和推送，已经是最新的了）
+	SYNC_NotDefined;
+
 }
