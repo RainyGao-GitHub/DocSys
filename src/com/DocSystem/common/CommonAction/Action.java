135151bcbf6cdb0a7f88aa4dca3d07e10170d808
yuan <13777479349@139.com>
2022-12-16 11:52:49 +0800

    syncUpDocChange maybe will no need again

--- src/com/DocSystem/common/CommonAction/Action.java
+++ src/com/DocSystem/common/CommonAction/Action.java
@@ -8,11 +8,11 @@ public enum Action {
 	MOVE,
 	COPY,
 	PUSH,
-	SYNC_ALL, 					//同步远程存储、版本仓库、强行刷新索引   （包括子目录）
-	SYNC_ALL_FORCE, 			//同步远程存储、版本仓库、强行刷新索引   （包括子目录）
-	SYNC_AUTO, 					//同步远程存储、版本仓库、不更新索引（包括子目录），不更新索引是为了保证性能
-	SYNC_AfterRevertHistory,	//同步版本仓库、更新有改动文件的索引（禁用远程拉取，避免恢复的文件被覆盖）
-	SYNC_AfterRemoteStoragePull,//同步版本仓库、更新有改动文件的索引（禁用远程拉取和推送，已经是最新的了）
+	SYNC_ALL, 					//同步版本仓库、同步远程存储、强行刷新SearchIndex
+	SYNC_ALL_FORCE, 			//同步版本仓库，同步远程存储、强行刷新SearchIndex
+	SYNC_AUTO, 					//同步版本仓库、同步远程存储（会更新文件索引）
+	SYNC_VerRepos,				//同步版本仓库（只是为了弥补文件操作版本提交失败的情况）
+	SYNC_RemoteStorage,			//同步远程存储（自动拉取）
 	SYNC_NotDefined;
 
 }
