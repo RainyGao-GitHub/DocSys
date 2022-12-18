4bf56eff6a3e643d87c4b43b5ebeb85be1d9d3c1
yuan <13777479349@139.com>
2022-12-18 19:27:33 +0800

    远程推送/自动备份: 异步执行
    1. copyDoc/copyDocRS/moveDoc/moveDocRS/deleteDoc/deleteDocRS

---
+++ src/com/DocSystem/common/ActionContext.java
@@ -0,0 +1,22 @@
+package com.DocSystem.common;
+
+import com.DocSystem.entity.Doc;
+import com.DocSystem.entity.Repos;
+import com.DocSystem.entity.User;
+
+public class ActionContext {
+
+	public String requestIP;
+
+	public User user;
+
+	public String event;
+	public String eventName;
+	public String subEvent;
+		
+	public Repos repos;
+	public Doc doc;
+	public Doc newDoc;
+	public Integer docLockType;
+	public Integer newDocLockType;
+}
