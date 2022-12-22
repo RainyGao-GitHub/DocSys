d8ed9fbbde90d2f919c399e40870324f21277a64
yuan <13777479349@139.com>
2022-12-22 10:11:34 +0800

    目录上传: add longBeatThreadCount for copySameDoc/createRealDoc/updateRealDoc

--- src/com/DocSystem/common/FolderUploadAction.java
+++ src/com/DocSystem/common/FolderUploadAction.java
@@ -6,38 +6,41 @@ import com.DocSystem.entity.User;
 
 public class FolderUploadAction {
 	public String actionId;
+
+	public String event;
+	public String subEvent;
+	public String eventName;
 	
 	public String requestIP;
+
+	public User user;
 
 	public Repos repos;			
 	public Doc doc;				//folder doc
 	public Integer docLockType;	//docLockType
 
-	public User user;
-
+	//Following data is for folderUpload
 	public String commitMsg;
 	public String commitUser;
-
+	
 	public String uploadLogPath; //存放subDocs upload info，上传结束时要写入系统日志中
 	public String localChangesRootPath; //上传成功的文件	
 	
-	public boolean isCriticalError = false;
-	public String errorInfo;
-
 	public long startTime;
-	public long beatTime;	//心跳时间（心跳停跳检测3分钟，但是checkDocInfo的大文件copy时间可能比较长，如何处理呢）
+	public long beatTime;	//心跳时间
 	public long beatStopThreshold = 3*6*1000; //默认3分钟
-	public boolean beatCheckIgnore = false;
+	public int longBeatThreadCount = 0;	//长心跳线程个数（分片上传的分片组合，checkDocInfo时的copySameDoc）
 	
-	//used for systemLog
-	public String event;
-	public String subEvent;
-	public String eventName;
-
 	//count info
 	public int totalCount;
 	public int successCount;
 	public int failCount;
 
+
+	
+	public boolean isCriticalError = false;
+	public String errorInfo;
+
 	public boolean stopFlag = false;
+	public long stopTime;
 }
