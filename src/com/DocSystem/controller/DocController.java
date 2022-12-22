65e3612692042760b938bc407fd1161e40cd161d
yuan <13777479349@139.com>
2022-12-22 21:10:22 +0800

    syncUpForDocChange: systemLog need write at the thread

--- src/com/DocSystem/controller/DocController.java
+++ src/com/DocSystem/controller/DocController.java
@@ -488,14 +488,14 @@ public class DocController extends BaseController{
 		}
 
 		writeJson(rt, response);
-		docSysDebugLog("refreshDoc() [" + doc.getPath() + doc.getName() + "]", rt);
-		addSystemLog(request, reposAccess.getAccessUser(), "refreshDoc", "refreshDoc", "刷新", "成功", repos, doc, null, buildSystemLogDetailContent(rt));
 
-		
+		docSysDebugLog("refreshDoc() [" + doc.getPath() + doc.getName() + "]", rt);
+
 		new Thread(new Runnable() {
 			public void run() {
 				Log.debug("refreshDoc() executeUniqueCommonActionList in new thread");
 				executeUniqueCommonActionList(actionList, rt);
+				addSystemLog(request, reposAccess.getAccessUser(), "refreshDoc", "refreshDoc", "刷新", "成功", repos, doc, null, buildSystemLogDetailContent(rt));
 			}
 		}).start();
 	}
