1cd7b231d675256af58aa229b9e94ae3328b1035
yuan <13777479349@139.com>
2023-01-24 17:22:42 +0800

    BugFix: 用户注册验证码邮件发送异常

--- src/com/DocSystem/controller/CommunityChannel.java
+++ src/com/DocSystem/controller/CommunityChannel.java
@@ -194,18 +194,12 @@ public class CommunityChannel extends BaseController implements Channel {
 
 	@Override
 	public String buildMailContent(String content) {
-		String Head = "尊敬的MxsDoc用户:";
-		String Tail = "如有任何问题，请联系 "
-				+ "<a href='mailto:helper@gofreeteam.com' style='text-decoration: none!important; text-decoration:none; color: #0064c8;' rel='noopener' target='_blank'>helper@gofreeteam.com</a>"
-				+ "<br>"
-				+ "<br>"
-				+ "谢谢,"
-				+ "<br>"
-				+ "<strong>MxsDoc团队</strong>"
-				+ "<br>"
-				+ "<a href='dw.gofreeteam.com' style='text-decoration: none!important; text-decoration:none; color: #0064c8;'>dw.gofreeteam.com</a>";
-				
-		return Head + content + Tail;
+		return null;
+	}
+
+	@Override
+	public String buildDocShareMailContent(String content) {
+		return null;
 	}
 
 
