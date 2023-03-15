e8a95aba5bedebe0779d7bf807ea0de46077217f
yuan <13777479349@139.com>
2023-03-15 16:35:08 +0800

    【优化】跨仓库推送多线程支持

--- src/com/DocSystem/common/entity/DocPushResult.java
+++ src/com/DocSystem/common/entity/DocPushResult.java
@@ -12,4 +12,8 @@ public class DocPushResult {
 	public boolean isSubAction = false;
 	public CommitAction action; //latest add commitAction
 	public String revision;	//svn和git会设置
+	public Object synclock;
+	public long retrySleepTime = 3*60*1000;	//3*60秒
+	public int threadCount = 0;
+	public int maxThreadCount = 10;
 }
