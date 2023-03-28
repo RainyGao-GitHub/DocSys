9bd0ad758dca91ff63d606d65343b04fc65054a0
yuan <13777479349@139.com>
2023-03-28 14:33:59 +0800

    UserPreferServer 增加 params 字段
    1. 用于兼容RemoteStorageConfig

--- src/com/DocSystem/common/entity/UserPreferServer.java
+++ src/com/DocSystem/common/entity/UserPreferServer.java
@@ -4,8 +4,9 @@ public class UserPreferServer {
 	public String id; //userId + serverUrl.hashCode + createTime 
 	
 	//UserInfo
-	public Integer userId;	//
-	public String userName; //
+	public Integer userId;	//创建人ID
+	public String userName; //创建人Name
+	public Long createTime; //创建日期
 	
 	//ServerInfo
 	public String serverType;	//MxsDoc/FTP/SFTP/SMB/GIT/SVN
@@ -13,8 +14,7 @@ public class UserPreferServer {
 	public String serverUrl;	//http://IP:port/DocSystem ftp://IP:port/path 
 	public String serverUserName;
 	public String serverUserPwd;
-	
-	public Long createTime;
+	public String params;	//扩展信息 key1=value1; key2=value2;
 	
 	//用于和RemoteStorageConfig兼容
 	public String url;	//For mxsdoc\git\svn		
@@ -22,7 +22,6 @@ public class UserPreferServer {
 	public Integer port; //For ftp/sftp/smb
 	public String charset;	//For ftp
 	public Integer passiveMode;	//For ftp 被动模式 0: 否 1: 是
-
 	public Integer isRemote; //For git/svn
 	public String localVerReposPath; //For git
 	public String serverUserDomain; //For smb
