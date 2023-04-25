a0790933e45def45586fa77e1944e8a83db78322
Rainy <652055239@qq.com>
2023-04-25 17:26:29 +0800

    Clean code

--- src/com/DocSystem/common/entity/LDAPConfig.java
+++ src/com/DocSystem/common/entity/LDAPConfig.java
@@ -8,8 +8,8 @@ public class LDAPConfig {
 	public String basedn;
 	public JSONObject settings;
 	public String authentication; //simple, GSSAPI
-	public Integer authMode;	//密码模式
-	public String loginMode;	//用户名字段属性
+	public Integer authMode;	//是否需要密码
+	public String loginMode;	//登录账户对应的查询字段
 	public String userAccount;	//LDAP管理员账号（如果指定了，则使用该用户鉴权）
 	public String userPassword;	//LDAP管理员的密码
 	public String filter;  		//过滤条件
