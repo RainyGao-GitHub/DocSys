1145e6f5c995a107304ba0f7a8c003d935fe4517
Rainy <652055239@qq.com>
2023-09-21 15:37:00 +0800

    【NewFeature】【多国语言支持】Sign In/Up/Out
    1. add addFirstAdminUser_en.html
    2. add register_en.html
    3. update en.js

--- WebRoot/web/lang/en.js
+++ WebRoot/web/lang/en.js
@@ -16,10 +16,20 @@ function lang(str)
 {
 	var translateMap = 
 	{
+		"服务器异常" : "Server Exception",			
 		"注册失败"	: "Sign Up Failed",			
 		"登录失败"	: "Sign In Failed",
 		"退出登录失败" : "Sign Out Failed",
-		"服务器异常" : "Server Exception",			
+		"获取验证码" : "Send Verify Code",
+		"验证码发送失败" : "Failed To Send Verify Code",
+		"验证码已发送" : "Verify Code was sent to you",
+		"请注意查收" : "Please check your email or SMS",
+		"添加系统管理员失败" : "Failed to add System Aministrator Account",
+		"两次密码输入不一致" : "Your re-enter password is incorrect",
+		"手机号不能为空" : "Mobile Number can not be empty",
+		"用户名不能为空" : "Account can not be empty",
+		"邮箱不能为空" : "Email can not be empty",
+		"密码不能为空" : "Password can not be empty",
 	};
 	
 	var newStr = langTypeMap[str];
