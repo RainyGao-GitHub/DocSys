3b257f388f3d76f3d19ea7d798134e51ca4d7d66
Rainy <652055239@qq.com>
2023-09-22 07:50:00 +0800

    【NewFeature】【多国语言支持】用户注册
    1. 后台返回错误信息翻译

--- WebRoot/web/lang/en.js
+++ WebRoot/web/lang/en.js
@@ -16,27 +16,38 @@ function lang(str)
 {
 	var translateMap = 
 	{
+		//通用
 		"服务器异常" : "Server Exception",			
+		//注册
 		"注册失败"	: "Sign Up Failed",			
-		"登录失败"	: "Sign In Failed",
-		"退出登录失败" : "Sign Out Failed",
 		"获取验证码" : "Send",
 		"验证码发送失败" : "Failed to send verification code",
 		"验证码已发送，请注意查收！" : "MxsDoc will send a verification code to your mobile phone or email!",
-		"添加系统管理员失败" : "Failed to add System Aministrator",
 		"两次密码输入不一致" : "Re-enter password error",
 		"手机号不能为空" : "Mobile Number is empty",
 		"用户名不能为空" : "Account is empty",
 		"邮箱不能为空" : "Email is empty",
 		"密码不能为空" : "Password is empty",
-		"数据库配置有变更，请先重启服务"	: "DataSource configuration was changed, Please restart server",
-		"系统初始化失败" : "System Init Failed",
 		"账号不能为空！" : "Account is empty!",
 		"账号格式不正确！" : "Account format error!",
 		"该用户已存在！" : "Account exists!",
+		"该手机已被使用！" : "Mobile Phone have been registered",
+		"该邮箱已被使用！" : "Email have been registered",
+		"手机格式错误！" : "Incorrect Mobile Number",
+		"邮箱格式错误！" : "Incorrect Email",
+		"验证码错误！" : "Incorrect verification code!",
 		"密码不能为空！" : "Password is empty!",
 		"两次密码不一致，请重试！" : "Re-enter password error!",
-		"验证码错误！" : "Incorrect verification code!",		
+		//登录
+		"登录失败"	: "Sign In Failed",
+		//退出登录
+		"退出登录失败" : "Sign Out Failed",
+		//添加系统管理员
+		"添加系统管理员失败" : "Failed to add System Aministrator",
+		//系统
+		"数据库配置有变更，请先重启服务"	: "DataSource configuration was changed, Please restart server",
+		"系统初始化失败" : "System Init Failed",
+				
 	};
 	
 	var newStr = translateMap[str];
