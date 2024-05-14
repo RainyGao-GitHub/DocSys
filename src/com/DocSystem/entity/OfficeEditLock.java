ccf861fbbbeb469bc0692fbad402f0e582593fad
Rainy <652055239@qq.com>
2024-05-14 22:46:12 +0800

    BugFix: RemoteStorageLock.java 和 OfficeEditLock.java 继承SyncSourceLock无法工作

--- src/com/DocSystem/entity/OfficeEditLock.java
+++ src/com/DocSystem/entity/OfficeEditLock.java
@@ -1,8 +1,34 @@
 package com.DocSystem.entity;
 
-public class OfficeEditLock extends SyncSourceLock {
+import java.io.Serializable;
+
+import com.DocSystem.common.SyncLock;
+
+public class OfficeEditLock implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5064183009607702364L;
+
+	public Integer id;
+
+	public Integer type;
+
+	public String name;	//lock name
+
+	public Integer state; 
+
+	public String locker = null;
+
+    public Integer lockBy = null;
+
+    public Long lockTime = null;
+    
+    public SyncLock synclock = null;
+
+	public String server;
+
+	public Long createTime;
+
+	public String info;	//lock info for debug
 }
--- src/com/DocSystem/entity/RemoteStorageLock.java
+++ src/com/DocSystem/entity/RemoteStorageLock.java
@@ -1,10 +1,35 @@
 package com.DocSystem.entity;
 
-public class RemoteStorageLock  extends SyncSourceLock {	
+import java.io.Serializable;
+
+import com.DocSystem.common.SyncLock;
+
+public class RemoteStorageLock  implements Serializable {	
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7436064038303366230L;
 
+	public Integer id;
+
+	public Integer type;
+
+	public String name;	//lock name
+
+	public Integer state; 
+
+	public String locker = null;
+
+    public Integer lockBy = null;
+
+    public Long lockTime = null;
+    
+    public SyncLock synclock = null;
+
+	public String server;
+
+	public Long createTime;
+
+	public String info;	//lock info for debug
 }
