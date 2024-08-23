7ccffcaafd0e2c50899e12fa4adfc08b7756c256
Rainy <652055239@qq.com>
2024-08-23 12:34:18 +0800

    Convert to utf8

--- src/com/DocSystem/test/FileUtilTest.java
+++ src/com/DocSystem/test/FileUtilTest.java
@@ -2,10 +2,30 @@ package com.DocSystem.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 import com.DocSystem.common.FileUtil;
 
 public class FileUtilTest {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception 
+	{
+		//test();
+		ConvertCharsetToUTF8("C:/N-20N3PF2E7EB0-Data/ragao/Desktop/", "docsystem_data.json", "docsystem_data-utf8.json");		
+	}
+	
+	static void ConvertCharsetToUTF8(String path, String name, String outputName) throws Exception
+	{
+		//读取文件的字符编码
+		String encode = FileUtil.getCharset(path + name);
+		
+		String content = FileUtil.readDocContentFromFile(path, name, encode);
+		
+		byte[] buff = content.getBytes("UTF-8");
+		FileUtil.saveDataToFile(buff, path, outputName);
+	}
+	
+	void test() throws Exception
+	{
 		String path = "C:/Test/";
 		String inName = "in.JPG";
 		String outName = "out.JPG";
@@ -13,6 +33,6 @@ public class FileUtilTest {
 		long size = file.length();
 		
 		byte[] buff = FileUtil.readBufferFromFile(path, inName, (long)0, (int)size);
-		FileUtil.saveDataToFile(buff, path, outName);	
-	}	
+		FileUtil.saveDataToFile(buff, path, outName);
+	}
 }
