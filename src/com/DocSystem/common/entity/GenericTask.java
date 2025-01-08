12e3a7229aa765797aba2ea2519e5ad7169fd283
Rainy <652055239@qq.com>
2025-01-08 16:41:44 +0800

    GenericTask 优化
    1. 保存调度任务，方便需要的时候进行清除

--- src/com/DocSystem/common/entity/GenericTask.java
+++ src/com/DocSystem/common/entity/GenericTask.java
@@ -1,6 +1,9 @@
 package com.DocSystem.common.entity;
 
+import java.util.concurrent.ScheduledFuture;
+
 public class GenericTask {
 	public Long createTime;
 	public boolean stopFlag = false;
+	public ScheduledFuture<?> scheduledFuture;
 }
--- src/com/DocSystem/controller/BaseController.java
+++ src/com/DocSystem/controller/BaseController.java
@@ -38,7 +38,6 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14548,7 +14547,7 @@ public class BaseController  extends BaseFunction{
 		clusterBeatTaskHashMap.put(curTime, task);
 		
 		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        executor.schedule(
+		task.scheduledFuture = executor.schedule(
         		new Runnable() {
         			long createTime = curTime;
         			
