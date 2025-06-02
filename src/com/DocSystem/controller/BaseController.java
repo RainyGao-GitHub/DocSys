b6a721357eabca2f3d82d02a77be479836eb54ce
Rainy <652055239@qq.com>
2025-06-02 12:08:52 +0800

    权限优先级检查没有进行null判断

--- src/com/DocSystem/controller/BaseController.java
+++ src/com/DocSystem/controller/BaseController.java
@@ -10658,7 +10658,7 @@ public class BaseController  extends BaseFunction{
 		}
 		else
 		{
-			if(docAuth.getPriority() >= parentPriority)
+			if(docAuth.getPriority() != null && docAuth.getPriority() >= parentPriority)
 			{
 				//Use the docAuth
 				return docAuth;
